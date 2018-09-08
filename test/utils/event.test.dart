29d149d2879fda87e09b0057538d0d6bee023b49
yohom <382146139@qq.com>
2018-09-08 14:34:20 +0800

    event做了一部分测试, 当isBehavior和seedValue同时设置的时候会有问题, 测试还没通过

--- test/utils/event.test.dart
+++ test/utils/event.test.dart
@@ -1,18 +1,69 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:framework/framework.dart';
-import 'package:mockito/mockito.dart';
 
 void main() {
-  group('构造器相关测试', () {
-    group('test参数与isDistinct参数', () {
-      test('test为null, isDistinct为true', () {
-        final event = Event<int>(test: null, isDistinct: true);
+  group('构造器相关测试:', () {
+    group('同步Event:', () {
+      test('其他默认配置, add数据后**同步**去取latest**应该**是add进去的数据', () {
+        final target = Event<int>(sync: true);
 
-        event.add(0);
-        event.add(0);
-        event.add(0);
+        target.add(1);
+        expect(target.latest, 1);
 
-//        verify(event.)
+        target.add(2);
+        expect(target.latest, 2);
+
+        target.add(3);
+        expect(target.latest, 3);
+      });
+
+      test('配置seedValue不为空, add数据后**同步**去取latest**应该**是add进去的数据', () {
+        final target = Event<String>(sync: true, seedValue: 'test_value');
+
+        target.add('1');
+        expect(target.latest, '1');
+
+        target.add('2');
+        expect(target.latest, '2');
+
+        target.add('3');
+        expect(target.latest, '3');
+      });
+
+      test(
+        '配置seedValue不为空 && isBehavior = true, '
+            'add数据后**同步**去取latest**应该**是add进去的数据',
+        () {
+          final target = Event<String>(
+            sync: true,
+            seedValue: 'test_value',
+            isBehavior: true,
+          );
+
+          target.add('1');
+          expect(target.latest, '1');
+
+          target.add('2');
+          expect(target.latest, '2');
+
+          target.add('3');
+          expect(target.latest, '3');
+        },
+      );
+    });
+
+    group('异步Event:', () {
+      test('其他默认配置, add数据后**同步**去取latest**不应该**是add进去的数据', () {
+        final target = Event<int>(sync: false);
+
+        target.add(1);
+        expect(target.latest != 1, true);
+
+        target.add(2);
+        expect(target.latest != 2, true);
+
+        target.add(3);
+        expect(target.latest != 3, true);
       });
     });
   });
