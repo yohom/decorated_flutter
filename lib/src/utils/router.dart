e99f2cbdd5c532f27473736fa1f73d57e6102ecd
yohom <382146139@qq.com>
2018-09-19 09:18:31 +0800

    整理

--- lib/src/utils/router.dart
+++ lib/src/utils/router.dart
@@ -5,9 +5,7 @@ import 'package:flutter/material.dart';
 import '../ui/loading.widget.dart';
 
 class Router {
-  ///
   /// 导航
-  ///
   static Future<T> navigate<T>(BuildContext context, Widget widget) {
     return Navigator.of(context).push<T>(
       MaterialPageRoute(
@@ -17,10 +15,8 @@ class Router {
     );
   }
 
-  ///
   /// 通过bottom sheet显示新界面, 通过这种方式, 新的界面会在父界面的节点下面, 而不是像
   /// [navigate]系列一样和父界面同级.
-  ///
   static PersistentBottomSheetController<T> navigateBottomSheet<T>(
     BuildContext context,
     Widget widget,
@@ -28,9 +24,7 @@ class Router {
     return showBottomSheet(context: context, builder: (context) => widget);
   }
 
-  ///
   /// 不保留源页面的跳转
-  ///
   static void navigateReplace(BuildContext context, Widget widget) {
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(
@@ -40,16 +34,12 @@ class Router {
     );
   }
 
-  ///
   /// 退出当前页
-  ///
   static void pop<T>(BuildContext context, [T data]) {
     Navigator.of(context).pop<T>(data);
   }
 
-  ///
   /// 退出到目标页
-  ///
   static void popTo<T>(BuildContext context, Type routeType) {
     Navigator.of(context).popUntil(
       (route) => route.settings.name == routeType.toString(),
@@ -66,9 +56,7 @@ class Router {
     );
   }
 
-  ///
   /// 等待页
-  ///
   static Future<T> loading<T>(BuildContext context, Future<T> futureTask) {
     showDialog(
       context: context,
