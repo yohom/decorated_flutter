630120864247cd52afd5f4c6df144b2e4432bca1
yohom <382146139@qq.com>
2018-09-19 16:36:17 +0800

    更换BLoCProvider实现

--- lib/src/ui/bloc_provider.dart
+++ lib/src/ui/bloc_provider.dart
@@ -1,102 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:framework/framework.dart';
+import 'package:framework/src/utils/bloc.dart';
 
-/// 局部BLoCProvider, 以和全局的BLoCProvider区分
-class LocalBLoCProvider<B extends BLoC> extends StatefulWidget {
-  final Widget child;
-  final B bloc;
-
-  LocalBLoCProvider({
+class BLoCProvider<T extends BLoC> extends StatefulWidget {
+  BLoCProvider({
     Key key,
     @required this.child,
     @required this.bloc,
-  }) : super(key: key);
+  }): super(key: key);
 
-  @override
-  _LocalBLoCProviderState createState() => _LocalBLoCProviderState();
-
-  static B of<B extends BLoC>(BuildContext context, [bool rebuild = true]) {
-    return (rebuild
-            ? context.inheritFromWidgetOfExactType(_LocalBLoCProvider)
-                as _LocalBLoCProvider
-            : context.ancestorWidgetOfExactType(_LocalBLoCProvider)
-                as _LocalBLoCProvider)
-        .bloc;
-  }
-}
-
-class _LocalBLoCProviderState extends State<LocalBLoCProvider> {
-  @override
-  Widget build(BuildContext context) {
-    return _LocalBLoCProvider(bloc: widget.bloc, child: widget.child);
-  }
-
-  @override
-  void dispose() {
-    widget.bloc.close();
-    super.dispose();
-  }
-}
-
-class _LocalBLoCProvider<B extends BLoC> extends InheritedWidget {
-  final B bloc;
-
-  _LocalBLoCProvider({
-    Key key,
-    @required this.bloc,
-    @required Widget child,
-  }) : super(key: key, child: child);
-
-  @override
-  bool updateShouldNotify(_LocalBLoCProvider old) => bloc != old.bloc;
-}
-
-/// 全局BLoCProvider, 以和局部的BLoCProvider区分
-class GlobalBLoCProvider<B extends BLoC> extends StatefulWidget {
+  final T bloc;
   final Widget child;
-  final B bloc;
-
-  GlobalBLoCProvider({
-    Key key,
-    @required this.child,
-    @required this.bloc,
-  }) : super(key: key);
 
   @override
-  _GlobalBLoCProviderState createState() => _GlobalBLoCProviderState();
+  _BlocProviderState<T> createState() => _BlocProviderState<T>();
 
-  static B of<B extends BLoC>(BuildContext context, [bool rebuild = true]) {
-    return (rebuild
-            ? context.inheritFromWidgetOfExactType(_GlobalBLoCProvider)
-                as _GlobalBLoCProvider
-            : context.ancestorWidgetOfExactType(_GlobalBLoCProvider)
-                as _GlobalBLoCProvider)
-        .bloc;
+  static T of<T extends BLoC>(BuildContext context){
+    final type = _typeOf<BLoCProvider<T>>();
+    BLoCProvider<T> provider = context.ancestorWidgetOfExactType(type);
+    return provider.bloc;
   }
+
+  static Type _typeOf<T>() => T;
 }
 
-class _GlobalBLoCProviderState extends State<GlobalBLoCProvider> {
+class _BlocProviderState<T> extends State<BLoCProvider<BLoC>>{
   @override
-  Widget build(BuildContext context) {
-    return _GlobalBLoCProvider(bloc: widget.bloc, child: widget.child);
-  }
-
-  @override
-  void dispose() {
+  void dispose(){
     widget.bloc.close();
     super.dispose();
   }
-}
-
-class _GlobalBLoCProvider<B extends BLoC> extends InheritedWidget {
-  final B bloc;
-
-  _GlobalBLoCProvider({
-    Key key,
-    @required this.bloc,
-    @required Widget child,
-  }) : super(key: key, child: child);
 
   @override
-  bool updateShouldNotify(_GlobalBLoCProvider old) => bloc != old.bloc;
+  Widget build(BuildContext context){
+    return widget.child;
+  }
 }
