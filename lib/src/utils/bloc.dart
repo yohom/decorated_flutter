df6a7877c2a6d53146e7a5710f8df0fd83eafe1c
yohom <382146139@qq.com>
2018-09-20 16:45:44 +0800

    BLoC增加semantics

--- lib/src/utils/bloc.dart
+++ lib/src/utils/bloc.dart
@@ -2,8 +2,12 @@ import 'package:framework/framework.dart';
 import 'package:meta/meta.dart';
 
 abstract class BLoC {
+  String semantics;
+
+  BLoC([this.semantics]);
+
   @mustCallSuper
   void close() {
-    L.p('$runtimeType closed');
+    L.p('${semantics ??= runtimeType.toString()} closed');
   }
 }
