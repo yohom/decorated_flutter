ea02d0150a148bbfa6dcb25b4c94a62940f4353f
yohom <382146139@qq.com>
2018-10-05 15:27:27 +0800

    优化ImageWidget

--- lib/src/ui/image.widget.dart
+++ lib/src/ui/image.widget.dart
@@ -1,20 +1,23 @@
-import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ImageWidget extends StatelessWidget {
-  final String imageUrl;
-  final BoxFit fit;
-
   const ImageWidget({
     @required this.imageUrl,
     this.fit = BoxFit.cover,
+    this.placeholder = const CupertinoActivityIndicator(),
   });
+
+  final String imageUrl;
+  final BoxFit fit;
+  final Widget placeholder;
 
   @override
   Widget build(BuildContext context) {
     return CachedNetworkImage(
       imageUrl: imageUrl,
-      placeholder: CircularProgressIndicator(),
+      placeholder: placeholder,
       errorWidget: Icon(Icons.error),
       fit: fit,
     );
