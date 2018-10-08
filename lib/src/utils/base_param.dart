164c7c0c8bbe6f15ba4960fbde16a014d4bdb43d
yohom <382146139@qq.com>
2018-10-08 16:03:49 +0800

    优化BaseParam

--- lib/src/utils/base_param.dart
+++ lib/src/utils/base_param.dart
@@ -1,10 +1,10 @@
 import 'package:framework/framework.dart';
 
 class BaseParam {
-  final _param = Map<String, String>();
+  final _param = Map<String, Object>();
 
   /// 加入单个参数
-  void putParam(String key, String value) {
+  void putParam(String key, Object value) {
     assert(isNotEmpty(key), 'key不能为null或者空字符串');
     assert(value != null, 'value不可以null, 可以为空字符串');
 
@@ -12,15 +12,13 @@ class BaseParam {
   }
 
   /// 加入一组参数
-  void putParamMap(Map<String, String> map) {
+  void putParamMap(Map<String, Object> map) {
     assert(map != null);
 
     _param.addAll(map);
   }
 
-  Map<String, String> get() {
-    return _param;
-  }
+  Map<String, Object> get() => _param;
 
   @override
   String toString() => _param.toString();
