7f805ab6f23520aaf220d2fab4a9470b99e68af5
yohom <382146139@qq.com>
2018-10-15 10:50:21 +0800

    增加FractionalScreen控件

---
+++ lib/src/ui/fractional_screen.widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:framework/framework.dart';
+
+class FractionalScreen extends StatelessWidget {
+  const FractionalScreen({
+    Key key,
+    this.fractionalWidth = double.infinity,
+    this.fractionalHeight = double.infinity,
+    this.child,
+  }) : super(key: key);
+
+  /// 占屏幕宽度百分比
+  final double fractionalWidth;
+
+  /// 占屏幕高度百分比
+  final double fractionalHeight;
+
+  /// child
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: Global.screenHeight * fractionalHeight,
+      height: Global.screenWidth * fractionalWidth,
+      child: child,
+    );
+  }
+}
