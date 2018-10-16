6040cea0d8e6d1e91189106fffea3aafae00f37d
yohom <382146139@qq.com>
2018-10-16 11:02:50 +0800

    FractionalScreen默认值改为1.0, 增加rowChildren和columnChildren

--- lib/src/ui/fractional_screen.widget.dart
+++ lib/src/ui/fractional_screen.widget.dart
@@ -4,10 +4,17 @@ import 'package:framework/framework.dart';
 class FractionalScreen extends StatelessWidget {
   const FractionalScreen({
     Key key,
-    this.widthFactor = double.infinity,
-    this.heightFactor = double.infinity,
+    this.widthFactor = 1.0,
+    this.heightFactor = 1.0,
     this.child,
-  }) : super(key: key);
+    this.columnChildren,
+    this.rowChildren,
+  })  : assert((child != null &&
+                rowChildren == null &&
+                columnChildren == null) ||
+            (child == null && rowChildren != null && columnChildren == null) ||
+            (child == null && rowChildren == null && columnChildren != null)),
+        super(key: key);
 
   /// 占屏幕宽度百分比
   final double widthFactor;
@@ -15,15 +22,25 @@ class FractionalScreen extends StatelessWidget {
   /// 占屏幕高度百分比
   final double heightFactor;
 
+  /// [child], [columnChildren], [rowChildren]三个只能设置一个
+  /// 如果都设置了, 那就按[child]->[columnChildren]->[rowChildren]的优先级作为child使用
   /// child
   final Widget child;
+
+  /// 子控件为Column的children
+  final List<Widget> columnChildren;
+
+  /// 子控件为Row的children
+  final List<Widget> rowChildren;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: Global.screenWidth * widthFactor,
       height: Global.screenHeight * heightFactor,
-      child: child,
+      child: child ??
+          Column(children: columnChildren) ??
+          Row(children: rowChildren),
     );
   }
 }
