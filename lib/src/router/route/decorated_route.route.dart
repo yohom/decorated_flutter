e21f2126159ca5b2bf6c81f23806d562632cc5e1
yohom <382146139@qq.com>
2018-10-30 10:55:59 +0800

    用overlay代替widget

--- lib/src/router/route/decorated_route.route.dart
+++ lib/src/router/route/decorated_route.route.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:framework/framework.dart';
-import 'package:framework/src/ui/runtime.widget.dart';
 
 typedef void _InitAction<T extends BLoC>(T bloc);
 
@@ -67,9 +66,32 @@ class DecoratedRoute<B extends BLoC> extends MaterialPageRoute {
     // 如果需要运行时信息不为空的话就加一层运行时信息显示
     // release模式任何情况下都不允许出现调试信息
     if (isNotEmpty(runtimeInfo) && !bool.fromEnvironment('dart.vm.product')) {
-      result = Runtime(
-        child: result,
-        runtimeInfo: runtimeInfo,
+      Overlay.of(context).insert(
+        OverlayEntry(
+          builder: (context) {
+            return Positioned(
+              bottom: kSpaceNormal,
+              child: Container(
+                height: Global.screenHeight / 3,
+                width: Global.screenWidth,
+                color: Colors.grey.withOpacity(0.6),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: runtimeInfo.length,
+                  itemBuilder: (context, index) {
+                    final event = runtimeInfo[index];
+                    return StreamBuilder(
+                      stream: event.stream,
+                      builder: (_, __) {
+                        return Text(event.runtimeSummary());
+                      },
+                    );
+                  },
+                ),
+              ),
+            );
+          },
+        ),
       );
     }
     return result;
