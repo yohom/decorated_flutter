5b84e315a815c32320946a06513cb42004b567e0
yohom <382146139@qq.com>
2018-10-30 14:35:05 +0800

    BLoC子类构造器

--- lib/src/bloc/bloc.dart
+++ lib/src/bloc/bloc.dart
@@ -20,6 +20,8 @@ abstract class BLoC {
 
 /// 顶层[BLoC], 这个[BLoC]只有子[BLoC], 没有[Event], 并且子[BLoC]都是[GlobalBLoC]
 abstract class RootBLoC extends BLoC {
+  RootBLoC([String semantics]) : super(semantics);
+
   List<GlobalBLoC> globalBLoCList = <GlobalBLoC>[];
 
   @override
@@ -32,6 +34,8 @@ abstract class RootBLoC extends BLoC {
 
 /// 局部[BLoC], 局部[BLoC]通常包在Screen的外面, 作用范围就是这个Screen内部
 abstract class LocalBLoC extends BLoC {
+  LocalBLoC([String semantics]) : super(semantics);
+
   /// 所有的event集合, 主要是提供给RuntimeScaffold使用
   List<Event> eventList = <Event>[];
 
@@ -45,6 +49,8 @@ abstract class LocalBLoC extends BLoC {
 
 /// 全局BLoC, 全局BLoC和局部BLoC目前没用什么功能上的区别, 只是做一下区分
 abstract class GlobalBLoC extends BLoC {
+  GlobalBLoC([String semantics]) : super(semantics);
+
   /// 所有的event集合, 主要是提供给RuntimeScaffold使用
   List<Event> eventList = <Event>[];
 
