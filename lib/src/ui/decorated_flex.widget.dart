f04d1bdb8c1ee4700f7053fb34746faadcc922f7
yohom <382146139@qq.com>
2018-10-31 10:39:28 +0800

    补完DecoratedRow/Column的参数

--- lib/src/ui/decorated_flex.widget.dart
+++ lib/src/ui/decorated_flex.widget.dart
@@ -5,24 +5,48 @@ class DecoratedRow extends StatelessWidget {
     Key key,
     this.padding,
     this.margin,
+    this.color,
+    this.decoration,
+    this.foregroundDecoration,
+    this.constraints,
+    this.transform,
+    this.width,
+    this.height,
+    this.alignment,
     this.mainAxisAlignment = MainAxisAlignment.start,
     this.mainAxisSize = MainAxisSize.max,
     this.crossAxisAlignment = CrossAxisAlignment.center,
     this.children,
   }) : super(key: key);
 
-  final List<Widget> children;
   final EdgeInsets padding;
   final EdgeInsets margin;
+  final Color color;
+  final Decoration decoration;
+  final Decoration foregroundDecoration;
+  final BoxConstraints constraints;
+  final Matrix4 transform;
+  final double width;
+  final double height;
+  final AlignmentGeometry alignment;
   final MainAxisAlignment mainAxisAlignment;
   final MainAxisSize mainAxisSize;
   final CrossAxisAlignment crossAxisAlignment;
+  final List<Widget> children;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: padding,
       margin: margin,
+      width: width,
+      height: height,
+      color: color,
+      decoration: decoration,
+      foregroundDecoration: foregroundDecoration,
+      constraints: constraints,
+      transform: transform,
+      alignment: alignment,
       child: Row(
         mainAxisAlignment: mainAxisAlignment,
         mainAxisSize: mainAxisSize,
@@ -32,30 +56,53 @@ class DecoratedRow extends StatelessWidget {
     );
   }
 }
-
 class DecoratedColumn extends StatelessWidget {
   const DecoratedColumn({
     Key key,
     this.padding,
     this.margin,
+    this.color,
+    this.decoration,
+    this.foregroundDecoration,
+    this.constraints,
+    this.transform,
+    this.width,
+    this.height,
+    this.alignment,
     this.mainAxisAlignment = MainAxisAlignment.start,
     this.mainAxisSize = MainAxisSize.max,
     this.crossAxisAlignment = CrossAxisAlignment.center,
     this.children,
   }) : super(key: key);
 
-  final List<Widget> children;
   final EdgeInsets padding;
   final EdgeInsets margin;
+  final Color color;
+  final Decoration decoration;
+  final Decoration foregroundDecoration;
+  final BoxConstraints constraints;
+  final Matrix4 transform;
+  final double width;
+  final double height;
+  final AlignmentGeometry alignment;
   final MainAxisAlignment mainAxisAlignment;
   final MainAxisSize mainAxisSize;
   final CrossAxisAlignment crossAxisAlignment;
+  final List<Widget> children;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: padding,
       margin: margin,
+      width: width,
+      height: height,
+      color: color,
+      decoration: decoration,
+      foregroundDecoration: foregroundDecoration,
+      constraints: constraints,
+      transform: transform,
+      alignment: alignment,
       child: Column(
         mainAxisAlignment: mainAxisAlignment,
         mainAxisSize: mainAxisSize,
