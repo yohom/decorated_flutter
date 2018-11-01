e7fa9f63bf81a5b8a21b70715a5935aa0279ba50
yohom <382146139@qq.com>
2018-11-01 11:58:18 +0800

    补完FutureListView的参数

--- lib/src/ui/future_list_view.widget.dart
+++ lib/src/ui/future_list_view.widget.dart
@@ -6,22 +6,45 @@ typedef Widget _ItemBuilder<T>(BuildContext context, T data);
 class FutureListView<T> extends StatelessWidget {
   const FutureListView({
     Key key,
-    this.future,
-    this.itemBuilder,
+    @required this.future,
+    @required this.itemBuilder,
     this.shrinkWrap = true,
+    this.showLoading = true,
+    this.initialData = const [],
+    this.emptyWidget,
+    this.errorWidget,
+    this.padding = const EdgeInsets.all(kSpaceLittleBig),
+    this.physics = const ClampingScrollPhysics(),
   }) : super(key: key);
 
+  //region FutureWidget
   final Future<List<T>> future;
+  final bool showLoading;
+  final List<T> initialData;
+  final Widget emptyWidget;
+  final Widget errorWidget;
+  //endregion
+  //region ListView.builder
   final _ItemBuilder itemBuilder;
   final bool shrinkWrap;
+  final EdgeInsets padding;
+  final ScrollPhysics physics;
+  //endregion
 
   @override
   Widget build(BuildContext context) {
     return FutureWidget<List<T>>(
       future: future,
+      showLoading: showLoading,
+      initialData: initialData,
+      emptyWidget: emptyWidget,
+      errorWidget: errorWidget,
       builder: (data) {
         return ListView.builder(
+          padding: padding,
           shrinkWrap: shrinkWrap,
+          physics: physics,
+          itemCount: data?.length ?? 0,
           itemBuilder: (context, index) => itemBuilder(context, data[index]),
         );
       },
