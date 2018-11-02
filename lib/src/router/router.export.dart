f53f95e1e930612d2df6a78d3cfe88102398f6af
yohom <382146139@qq.com>
2018-11-02 17:53:36 +0800

    整理

--- lib/src/router/router.export.dart
+++ lib/src/router/router.export.dart
@@ -1,6 +1,4 @@
-export 'package:framework/src/router/route/decorated_route.route.dart';
+export 'package:framework/src/ui/route/decorated_route.route.dart';
 
-export 'route/custom_material.route.dart';
 export 'route_parser.dart';
 export 'router.dart';
-export 'unknown_route.screen.dart';
