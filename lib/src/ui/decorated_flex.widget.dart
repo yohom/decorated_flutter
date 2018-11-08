102332b46d690c45b59bc004545d7b4321de7355
yohom <382146139@qq.com>
2018-11-08 09:53:52 +0800

    Revert "DecoratedRow增加borderRadius"
    This reverts commit ffb42aa106e447790d80ad96f841ea0cccf9e217.

--- lib/src/ui/decorated_flex.widget.dart
+++ lib/src/ui/decorated_flex.widget.dart
@@ -19,7 +19,6 @@ class DecoratedRow extends StatelessWidget {
     this.onTap,
     this.onLongPress,
     this.behavior = HitTestBehavior.opaque,
-    this.borderRadius,
     this.children,
   }) : super(key: key);
 
@@ -33,52 +32,42 @@ class DecoratedRow extends StatelessWidget {
   final Matrix4 transform;
   final double width;
   final double height;
-
   //endregion
   //region Row
   final AlignmentGeometry alignment;
   final MainAxisAlignment mainAxisAlignment;
   final MainAxisSize mainAxisSize;
   final CrossAxisAlignment crossAxisAlignment;
-
   //endregion
   //region GestureDetector
   final VoidCallback onTap;
   final VoidCallback onLongPress;
   final HitTestBehavior behavior;
-
-  //endregion
-  //region ClipRRect
-  final BorderRadius borderRadius;
-
   //endregion
   final List<Widget> children;
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: borderRadius ?? BorderRadius.circular(0.0),
-      child: GestureDetector(
-        behavior: behavior,
-        onTap: onTap,
-        onLongPress: onLongPress,
-        child: Container(
-          padding: padding,
-          margin: margin,
-          width: width,
-          height: height,
-          color: color,
-          decoration: decoration,
-          foregroundDecoration: foregroundDecoration,
-          constraints: constraints,
-          transform: transform,
-          alignment: alignment,
-          child: Row(
-            mainAxisAlignment: mainAxisAlignment,
-            mainAxisSize: mainAxisSize,
-            crossAxisAlignment: crossAxisAlignment,
-            children: children,
-          ),
+    return GestureDetector(
+      behavior: behavior,
+      onTap: onTap,
+      onLongPress: onLongPress,
+      child: Container(
+        padding: padding,
+        margin: margin,
+        width: width,
+        height: height,
+        color: color,
+        decoration: decoration,
+        foregroundDecoration: foregroundDecoration,
+        constraints: constraints,
+        transform: transform,
+        alignment: alignment,
+        child: Row(
+          mainAxisAlignment: mainAxisAlignment,
+          mainAxisSize: mainAxisSize,
+          crossAxisAlignment: crossAxisAlignment,
+          children: children,
         ),
       ),
     );
@@ -103,8 +92,6 @@ class DecoratedColumn extends StatelessWidget {
     this.crossAxisAlignment = CrossAxisAlignment.center,
     this.onTap,
     this.onLongPress,
-    this.behavior = HitTestBehavior.opaque,
-    this.borderRadius,
     this.children,
   }) : super(key: key);
 
@@ -118,52 +105,40 @@ class DecoratedColumn extends StatelessWidget {
   final Matrix4 transform;
   final double width;
   final double height;
-
   //endregion
   //region Row
   final AlignmentGeometry alignment;
   final MainAxisAlignment mainAxisAlignment;
   final MainAxisSize mainAxisSize;
   final CrossAxisAlignment crossAxisAlignment;
-
   //endregion
   //region GestureDetector
   final VoidCallback onTap;
   final VoidCallback onLongPress;
-  final HitTestBehavior behavior;
-
-  //endregion
-  //region ClipRRect
-  final BorderRadius borderRadius;
-
   //endregion
   final List<Widget> children;
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: borderRadius ?? BorderRadius.circular(0.0),
-      child: GestureDetector(
-        behavior: behavior,
-        onTap: onTap,
-        onLongPress: onLongPress,
-        child: Container(
-          padding: padding,
-          margin: margin,
-          width: width,
-          height: height,
-          color: color,
-          decoration: decoration,
-          foregroundDecoration: foregroundDecoration,
-          constraints: constraints,
-          transform: transform,
-          alignment: alignment,
-          child: Column(
-            mainAxisAlignment: mainAxisAlignment,
-            mainAxisSize: mainAxisSize,
-            crossAxisAlignment: crossAxisAlignment,
-            children: children,
-          ),
+    return GestureDetector(
+      onTap: onTap,
+      onLongPress: onLongPress,
+      child: Container(
+        padding: padding,
+        margin: margin,
+        width: width,
+        height: height,
+        color: color,
+        decoration: decoration,
+        foregroundDecoration: foregroundDecoration,
+        constraints: constraints,
+        transform: transform,
+        alignment: alignment,
+        child: Column(
+          mainAxisAlignment: mainAxisAlignment,
+          mainAxisSize: mainAxisSize,
+          crossAxisAlignment: crossAxisAlignment,
+          children: children,
         ),
       ),
     );
