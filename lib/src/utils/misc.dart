853f2421d273fba4909627f169e6afdf130740f8
yohom <382146139@qq.com>
2018-11-16 13:12:43 +0800

    bugfix: loading的返回逻辑优化

--- lib/src/utils/misc.dart
+++ lib/src/utils/misc.dart
@@ -41,15 +41,24 @@ void handleError(BuildContext context, Object error) {
 
 /// 等待页
 Future<T> loading<T>(BuildContext context, Future<T> futureTask) {
+  // 是被future pop的还是按返回键pop的
+  bool popByFuture = true;
+
   showDialog(
     context: context,
     builder: (context) {
       return WillPopScope(onWillPop: () async => false, child: LoadingWidget());
     },
     barrierDismissible: false,
-  );
+  ).whenComplete(() {
+    // 1. 如果是返回键pop的, 那么设置成true, 这样future完成时就不会pop了
+    // 2. 如果是future完成导致的pop, 那么这一行是没用任何作用的
+    popByFuture = false;
+  });
   return futureTask.whenComplete(() {
     // 由于showDialog会强制使用rootNavigator, 所以这里pop的时候也要用rootNavigator
-    Navigator.of(context, rootNavigator: true).pop(context);
+    if (popByFuture) {
+      Navigator.of(context, rootNavigator: true).pop(context);
+    }
   });
 }
