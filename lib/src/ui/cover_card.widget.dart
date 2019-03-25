a8615c201bf9dbf0327890fa9de6f9b114ff88b1
yohom <382146139@qq.com>
2019-03-25 16:17:18 +0800

    feature: cover_card初步, shadowed_box默认参数调整

--- lib/src/ui/cover_card.widget.dart
+++ lib/src/ui/cover_card.widget.dart
@@ -1,7 +1,8 @@
+import 'package:decorated_flutter/decorated_flutter.dart';
 import 'package:flutter/material.dart';
 
-class ParallaxBox extends StatefulWidget {
-  const ParallaxBox({
+class CoverCard extends StatefulWidget {
+  const CoverCard({
     Key key,
     this.background,
     this.foreground,
@@ -10,6 +11,9 @@ class ParallaxBox extends StatefulWidget {
     this.fit = StackFit.loose,
     this.overflow = Overflow.clip,
     this.parallaxRate = 0.2,
+    @required this.cover,
+    @required this.card,
+    @required this.coverHeight,
   })  : assert(parallaxRate > 0.0),
         super(key: key);
 
@@ -34,11 +38,20 @@ class ParallaxBox extends StatefulWidget {
   /// 视差率, 也就是前景移动时, 背景移动的速率
   final double parallaxRate;
 
+  /// 封面
+  final Widget cover;
+
+  /// 内容Card
+  final Widget card;
+
+  /// 封面高度
+  final double coverHeight;
+
   @override
-  _ParallaxBoxState createState() => _ParallaxBoxState();
+  _CoverCardState createState() => _CoverCardState();
 }
 
-class _ParallaxBoxState extends State<ParallaxBox> {
+class _CoverCardState extends State<CoverCard> {
   final _controller = ScrollController();
 
   double _parallaxOffset = 0.0;
@@ -61,11 +74,20 @@ class _ParallaxBoxState extends State<ParallaxBox> {
       children: <Widget>[
         Transform.translate(
           offset: Offset(0.0, _parallaxOffset),
-          child: widget.background,
+          child: Container(
+            height: widget.coverHeight,
+            child: widget.cover,
+          ),
         ),
         SingleChildScrollView(
           controller: _controller,
-          child: widget.foreground,
+          child: FractionalScreen(
+            heightFactor: 1,
+            children: <Widget>[
+              Container(height: widget.coverHeight),
+              widget.card,
+            ],
+          ),
         ),
         widget.overlay,
       ],
