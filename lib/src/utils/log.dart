b474f848c749a44da332cf139120f472890fe8cf
yohom <yohombao@gmail.com>
2020-02-09 18:10:31 +0800

    enhance: 去除logging库

--- lib/src/utils/log.dart
+++ lib/src/utils/log.dart
@@ -1,39 +1,29 @@
 import 'package:flutter/foundation.dart';
-import 'package:logging/logging.dart';
 
 final _Logger L = _Logger();
 
 class _Logger {
-  _Logger() {
-    Logger.root.level = Level.ALL; // defaults to Level.INFO
-    Logger.root.onRecord.listen((record) {
-      debugPrint('${record.level.name}: ${record.time}: ${record.message}');
-    });
-  }
-
-  final log = Logger('Logger');
-
   void d(Object content) {
     if (!kReleaseMode) {
-      log.fine(content.toString());
+      debugPrint(content.toString());
     }
   }
 
   void w(Object content) {
     if (!kReleaseMode) {
-      log.warning(content.toString());
+      debugPrint(content.toString());
     }
   }
 
   void i(Object content) {
     if (!kReleaseMode) {
-      log.info(content.toString());
+      debugPrint(content.toString());
     }
   }
 
   void e(Object content) {
     if (!kReleaseMode) {
-      log.severe(content.toString());
+      debugPrint(content.toString());
     }
   }
 
@@ -41,13 +31,13 @@ class _Logger {
   @Deprecated('使用L.d代替')
   void p(Object content) {
     if (!kReleaseMode) {
-      log.fine(content.toString());
+      debugPrint(content.toString());
     }
   }
 
   /// release模式打印
   @Deprecated('使用L.d代替')
   void r(Object content) {
-    log.fine(content.toString());
+    debugPrint(content.toString());
   }
 }
