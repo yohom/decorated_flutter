8ad57c58954bedb68b4e3221eadbb6064dfef4dc
yohom <382146139@qq.com>
2020-07-22 14:15:46 +0800

    feat: 增加show_more_text

---
+++ lib/src/ui/widget/visual/special_affect/show_more_text.widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ShowMoreText extends StatelessWidget {
+  const ShowMoreText(
+    this.text, {
+    Key key,
+    this.maxLines = 1,
+    this.style,
+    this.showMoreStyle,
+    this.overflow = TextOverflow.ellipsis,
+    this.popupMenuTheme = const PopupMenuThemeData(),
+  }) : super(key: key);
+
+  final String text;
+  final int maxLines;
+  final TextStyle style;
+  final TextStyle showMoreStyle;
+  final TextOverflow overflow;
+  final PopupMenuThemeData popupMenuTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(popupMenuTheme: popupMenuTheme),
+      child: PopupMenuButton(
+        child: Text(
+          text,
+          style: style,
+          maxLines: maxLines,
+          overflow: overflow,
+        ),
+        itemBuilder: (context) {
+          return [
+            PopupMenuItem(
+              child: Text(text, style: showMoreStyle),
+            ),
+          ];
+        },
+      ),
+    );
+  }
+}
