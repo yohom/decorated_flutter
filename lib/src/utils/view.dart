af14e7092f04ad4de62dde099269b9f27562f42b
yohom <382146139@qq.com>
2020-08-04 16:37:49 +0800

    enhance: toast去掉默认值

--- lib/src/utils/view.dart
+++ lib/src/utils/view.dart
@@ -1,17 +1,11 @@
-import 'package:decorated_flutter/decorated_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:oktoast/oktoast.dart';
-
-const _duration = 2000;
 
 void toast(
   String message, {
   ToastPosition position = ToastPosition.center,
-  double radius = 32,
-  EdgeInsets padding = const EdgeInsets.symmetric(
-    vertical: kSpaceLittleBig,
-    horizontal: kSpaceBig,
-  ),
+  double radius,
+  EdgeInsets padding,
   TextStyle textStyle,
   VoidCallback onDismiss,
   bool dismissOtherToast = true,
