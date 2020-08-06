2a8500a168c1d085017fbd116a33ab2301e768e9
yohom <382146139@qq.com>
2020-08-06 20:59:55 +0800

    enhance: LoadingWidget增加height参数

--- lib/src/ui/widget/visual/placeholder/loading.widget.dart
+++ lib/src/ui/widget/visual/placeholder/loading.widget.dart
@@ -2,10 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({Key key, this.height}) : super(key: key);
+
+  final double height;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
+    Widget result = Center(
       child: CupertinoActivityIndicator(),
     );
+    if (height != null) {
+      result = SizedBox(height: height, child: result);
+    }
+    return result;
   }
 }
