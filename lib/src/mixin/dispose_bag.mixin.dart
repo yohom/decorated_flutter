ea7397ab3426f465d707c2dfb285b8d05a8ad858
yohom <382146139@qq.com>
2020-08-21 19:50:03 +0800

    enhance: 增加BLoCDisposeBag

--- lib/src/mixin/dispose_bag.mixin.dart
+++ lib/src/mixin/dispose_bag.mixin.dart
@@ -1,8 +1,19 @@
 import 'dart:async';
 
+import 'package:decorated_flutter/src/bloc/bloc.export.dart';
 import 'package:flutter/cupertino.dart';
 
 mixin DisposeBag<T extends StatefulWidget> on State<T> {
+  List<StreamSubscription> disposeBag = [];
+
+  @override
+  void dispose() {
+    disposeBag.forEach((it) => it.cancel());
+    super.dispose();
+  }
+}
+
+mixin BLoCDisposeBag on BLoC {
   List<StreamSubscription> disposeBag = [];
 
   @override
--- lib/src/mixin/mixin.export.dart
+++ lib/src/mixin/mixin.export.dart
@@ -1,2 +1,1 @@
-export 'aysnc.mixin.dart';
 export 'dispose_bag.mixin.dart';
