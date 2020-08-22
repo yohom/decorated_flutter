81762bada251fecea19927324c4332c4e9a34039
yohom <382146139@qq.com>
2020-08-22 20:59:19 +0800

    enhance: 增加loading灵活性

--- lib/src/extension/future.x.dart
+++ lib/src/extension/future.x.dart
@@ -6,27 +6,48 @@ typedef Widget LoadingBuilder(BuildContext context, String loadingText);
 
 extension FutureX<T> on Future<T> {
   static LoadingBuilder loadingWidgetBuilder;
+  static Color backgroundColor;
 
   Future<T> loading(
     BuildContext context, {
     bool cancelable = false,
     Duration timeout = const Duration(seconds: 20),
     String loadingText = '加载中..',
+    Color backgroundColor,
   }) {
     // 是被future pop的还是按返回键pop的
     bool popByFuture = true;
 
-    showDialog(
+    final ThemeData theme = Theme.of(context, shadowThemeOnly: true);
+    showGeneralDialog(
       context: context,
-      builder: (context) {
-        return WillPopScope(
-          onWillPop: () async => cancelable,
-          child: loadingWidgetBuilder != null
-              ? loadingWidgetBuilder(context, loadingText)
-              : LoadingWidget(),
+      pageBuilder: (
+        BuildContext buildContext,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+      ) {
+        final Widget pageChild = Builder(
+          builder: (context) {
+            return WillPopScope(
+              onWillPop: () async => cancelable,
+              child: loadingWidgetBuilder != null
+                  ? loadingWidgetBuilder(context, loadingText)
+                  : LoadingWidget(),
+            );
+          },
+        );
+        return Builder(
+          builder: (BuildContext context) {
+            return theme != null
+                ? Theme(data: theme, child: pageChild)
+                : pageChild;
+          },
         );
       },
+      transitionDuration: const Duration(milliseconds: 150),
       barrierDismissible: cancelable,
+      barrierColor:
+          backgroundColor ?? FutureX.backgroundColor ?? Colors.black54,
     ).whenComplete(() {
       // 1. 如果是返回键pop的, 那么设置成true, 这样future完成时就不会pop了
       // 2. 如果是future完成导致的pop, 那么这一行是没用任何作用的
