993351236862ed39faaf0184205ed2bbb1d2f75f
yohom <382146139@qq.com>
2020-08-27 21:32:04 +0800

    enhance: Static允许null

--- lib/src/bloc/bloc_io.dart
+++ lib/src/bloc/bloc_io.dart
@@ -119,10 +119,6 @@ class Static<T> {
   T _content;
 
   void set(T value) {
-    assert(value != null);
-    if (value == null) {
-      throw 'Static值不能为null';
-    }
     _content = value;
   }
 
