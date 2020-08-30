f3168a1ff19f0f247f4eb4c3fe6f19988d98cf43
yohom <382146139@qq.com>
2020-08-30 08:53:12 +0800

    feat: 增加RenderRepaintBoundaryX

---
+++ lib/src/extension/render_repaint_boundary.x.dart
@@ -0,0 +1,12 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:flutter/rendering.dart';
+
+extension RenderRepaintBoundaryX on RenderRepaintBoundary {
+  Future<Uint8List> capture(double devicePixelRatio) async {
+    return toImage(pixelRatio: devicePixelRatio)
+        .then((image) => image.toByteData(format: ImageByteFormat.png))
+        .then((byteData) => byteData.buffer.asUint8List());
+  }
+}
