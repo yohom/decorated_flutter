99668003859a607269406403305f3c018e38ac9b
yohom <382146139@qq.com>
2020-10-17 09:35:27 +0800

    enhance: 增加SliverLoadingWidget

--- lib/src/ui/widget/visual/placeholder/loading.widget.dart
+++ lib/src/ui/widget/visual/placeholder/loading.widget.dart
@@ -18,3 +18,25 @@ class LoadingWidget extends StatelessWidget {
     return result;
   }
 }
+
+class SliverLoadingWidget extends StatelessWidget {
+  const SliverLoadingWidget({
+    Key key,
+    this.height,
+    this.width,
+  }) : super(key: key);
+
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget result = Center(
+      child: CupertinoActivityIndicator(),
+    );
+    if (height != null || width != null) {
+      result = SizedBox(height: height, width: width, child: result);
+    }
+    return SliverToBoxAdapter(child: result);
+  }
+}
