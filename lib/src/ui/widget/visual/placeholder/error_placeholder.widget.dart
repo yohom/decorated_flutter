a08e4c5515d65b6fd66d8bedefc384eb0e8700a0
yohom <382146139@qq.com>
2020-10-19 19:22:40 +0800

    enhance: Subscriber增加sliver参数

--- lib/src/ui/widget/visual/placeholder/error_placeholder.widget.dart
+++ lib/src/ui/widget/visual/placeholder/error_placeholder.widget.dart
@@ -2,29 +2,23 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ErrorPlaceholder extends StatelessWidget {
-  const ErrorPlaceholder({Key key}) : super(key: key);
+  const ErrorPlaceholder({
+    Key key,
+    this.sliver = false,
+  }) : super(key: key);
+
+  final bool sliver;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    Widget result = Center(
       child: kDebugMode
           ? Icon(Icons.error_outline, color: Colors.red)
           : Container(),
     );
-  }
-}
-
-class SliverErrorPlaceholder extends StatelessWidget {
-  const SliverErrorPlaceholder({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Center(
-        child: kDebugMode
-            ? Icon(Icons.error_outline, color: Colors.red)
-            : Container(),
-      ),
-    );
+    if (sliver) {
+      result = SliverToBoxAdapter(child: result);
+    }
+    return result;
   }
 }
--- lib/src/ui/widget/visual/placeholder/loading.widget.dart
+++ lib/src/ui/widget/visual/placeholder/loading.widget.dart
@@ -2,32 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoadingWidget extends StatelessWidget {
-  const LoadingWidget({Key key, this.height, this.width}) : super(key: key);
-
-  final double width;
-  final double height;
-
-  @override
-  Widget build(BuildContext context) {
-    Widget result = Center(
-      child: CupertinoActivityIndicator(),
-    );
-    if (height != null || width != null) {
-      result = SizedBox(height: height, width: width, child: result);
-    }
-    return result;
-  }
-}
-
-class SliverLoadingWidget extends StatelessWidget {
-  const SliverLoadingWidget({
+  const LoadingWidget({
     Key key,
     this.height,
     this.width,
+    this.sliver = false,
   }) : super(key: key);
 
   final double width;
   final double height;
+  final bool sliver;
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +21,9 @@ class SliverLoadingWidget extends StatelessWidget {
     if (height != null || width != null) {
       result = SizedBox(height: height, width: width, child: result);
     }
-    return SliverToBoxAdapter(child: result);
+    if (sliver) {
+      result = SliverToBoxAdapter(child: result);
+    }
+    return result;
   }
 }
