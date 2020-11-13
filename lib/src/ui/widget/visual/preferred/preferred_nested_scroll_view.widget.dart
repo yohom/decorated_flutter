58f43a270ee624a6e217004adf32cfb06df978b9
yohom <382146139@qq.com>
2020-11-13 18:48:49 +0800

    enhance: 优化PreferredNestedScrollView参数

--- lib/src/ui/widget/visual/preferred/preferred_nested_scroll_view.widget.dart
+++ lib/src/ui/widget/visual/preferred/preferred_nested_scroll_view.widget.dart
@@ -3,18 +3,20 @@ import 'package:flutter/material.dart';
 class PreferredNestedScrollView extends StatelessWidget {
   const PreferredNestedScrollView({
     Key key,
-    @required this.header,
+    @required this.headers,
     @required this.body,
   }) : super(key: key);
 
-  final Widget header;
+  final List<Widget> headers;
   final Widget body;
 
   @override
   Widget build(BuildContext context) {
     return NestedScrollView(
       headerSliverBuilder: (context, innerBoxIsScrolled) {
-        return [SliverToBoxAdapter(child: header)];
+        return [
+          for (final header in headers) SliverToBoxAdapter(child: header)
+        ];
       },
       body: body,
     );
