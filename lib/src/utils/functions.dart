928b94c5f693862cc4d1047a2f1d721b99c9a614
yohom <382146139@qq.com>
2020-11-15 10:25:19 +0800

    enhance: 增加select方法

--- lib/src/utils/functions.dart
+++ lib/src/utils/functions.dart
@@ -5,8 +5,3 @@ final notNull = (data) => data != null;
 final notEmpty = (data) => isNotEmpty(data);
 final isTrue = (bool data) => data == true;
 final isFalse = (bool data) => data == false;
-
-/// 从集合中*单选*出目标[target]项
-final Function radio = (Selectable data) {
-  return (dynamic target) => data.selected = (data == target);
-};
