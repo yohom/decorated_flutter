b26ae006858934459a26e10f828791a029f4ad9d
yohom <382146139@qq.com>
2020-12-01 21:34:03 +0800

    enhance: 增加getOrNull

--- lib/src/extension/list.x.dart
+++ lib/src/extension/list.x.dart
@@ -1,1 +1,10 @@
-extension ListX<T> on List<T> {}
+extension ListX<T> on List<T> {
+  T getOrNull(int index) {
+    try {
+      final result = this[index];
+      return result;
+    } catch (e) {
+      return null;
+    }
+  }
+}
