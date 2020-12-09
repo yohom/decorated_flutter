34603637f373d00ee562f8bf812543ff3e961263
yohom <382146139@qq.com>
2020-12-09 21:43:51 +0800

    enhance: 增加onPressed

--- lib/src/ui/widget/visual/special_affect/countdown.widget.dart
+++ lib/src/ui/widget/visual/special_affect/countdown.widget.dart
@@ -1,3 +1,4 @@
+import 'package:decorated_flutter/decorated_flutter.dart';
 import 'package:flutter/material.dart';
 
 class Countdown extends StatelessWidget {
@@ -6,15 +7,17 @@ class Countdown extends StatelessWidget {
     @required this.initialData,
     this.duration = const Duration(seconds: 1),
     @required this.builder,
+    this.onPressed,
   }) : super(key: key);
 
   final int initialData;
   final Duration duration;
   final Widget Function(BuildContext, int) builder;
+  final ContextCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<int>(
+    Widget result = StreamBuilder<int>(
       initialData: initialData,
       stream: Stream.periodic(duration, (count) => initialData - 1 - count)
           .take(initialData),
@@ -22,5 +25,11 @@ class Countdown extends StatelessWidget {
         return builder?.call(context, snapshot.data);
       },
     );
+
+    if (onPressed != null) {
+      result = GestureDetector(onTap: () => onPressed(context), child: result);
+    }
+
+    return result;
   }
 }
