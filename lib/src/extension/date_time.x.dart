11500199a89bb812f097c30190c246ef6eb844e6
yohom <382146139@qq.com>
2020-12-19 09:56:29 +0800

    enhance: 增加isTomorrow, isYesterday, isToday

--- lib/src/extension/date_time.x.dart
+++ lib/src/extension/date_time.x.dart
@@ -12,4 +12,22 @@ extension DateTimeX on DateTime {
   String format([String format = 'yyyy-MM-dd HH:mm:ss']) {
     return DateFormat(format).format(this);
   }
+
+  bool isToday() {
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day);
+    return DateTime(year, month, day) == today;
+  }
+
+  bool isYesterday() {
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day - 1);
+    return DateTime(year, month, day) == today;
+  }
+
+  bool isTomorrow() {
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day + 1);
+    return DateTime(year, month, day) == today;
+  }
 }
