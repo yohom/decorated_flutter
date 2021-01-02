93e5370d08d998025a4492226835bd4d5851853b
yohom <382146139@qq.com>
2021-01-02 23:02:09 +0800

    enhance: shape.widget.dart增加padding等参数

--- lib/src/ui/widget/visual/preferred/shape.widget.dart
+++ lib/src/ui/widget/visual/preferred/shape.widget.dart
@@ -5,17 +5,26 @@ class Circle extends StatelessWidget {
     Key key,
     @required this.radius,
     @required this.color,
+    this.padding,
+    this.margin,
+    this.child,
   }) : super(key: key);
 
   final double radius;
   final Color color;
+  final EdgeInsets padding;
+  final EdgeInsets margin;
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: radius,
       height: radius,
+      padding: padding,
+      margin: margin,
       decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+      child: child,
     );
   }
 }
@@ -25,17 +34,26 @@ class Square extends StatelessWidget {
     Key key,
     @required this.side,
     @required this.color,
+    this.padding,
+    this.margin,
+    this.child,
   }) : super(key: key);
 
   final double side;
   final Color color;
+  final EdgeInsets padding;
+  final EdgeInsets margin;
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: side,
       height: side,
+      padding: padding,
+      margin: margin,
       decoration: BoxDecoration(color: color, shape: BoxShape.rectangle),
+      child: child,
     );
   }
 }
