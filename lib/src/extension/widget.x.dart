07facd9147fdbb816a3cb79722fd79ca6fb0586f
yohom <382146139@qq.com>
2021-01-02 15:42:41 +0800

    enhance: image.x.dart -> widget.x.dart

--- lib/src/extension/widget.x.dart
+++ lib/src/extension/widget.x.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 
-extension ImageX on Image {
+extension WidgetX on Widget {
   Text operator +(InlineSpan otherSpan) {
     final imageSpan = WidgetSpan(child: this);
     return Text.rich(
