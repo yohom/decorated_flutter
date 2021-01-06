756176add51d41406cbb487047b82cdce69e1aa7
yohom <382146139@qq.com>
2021-01-06 20:04:12 +0800

    enhance: 增加fillRemaining

--- lib/src/ui/widget/visual/decorated/decorated_flex.widget.dart
+++ lib/src/ui/widget/visual/decorated/decorated_flex.widget.dart
@@ -49,6 +49,7 @@ class DecoratedRow extends DecoratedFlex {
     bool center,
     bool sliver = false,
     VerticalDirection verticalDirection,
+    bool fillRemaining,
     List<Widget> children,
   }) : super(
           key: key,
@@ -95,6 +96,7 @@ class DecoratedRow extends DecoratedFlex {
           center: center,
           sliver: sliver,
           verticalDirection: verticalDirection,
+          fillRemaining: fillRemaining,
           children: children,
         );
 }
@@ -144,6 +146,7 @@ class DecoratedColumn extends DecoratedFlex {
     bool center,
     bool sliver = false,
     VerticalDirection verticalDirection,
+    bool fillRemaining,
     List<Widget> children,
   }) : super(
           key: key,
@@ -190,6 +193,7 @@ class DecoratedColumn extends DecoratedFlex {
           center: center,
           sliver: sliver,
           verticalDirection: verticalDirection,
+          fillRemaining: fillRemaining,
           children: children,
         );
 }
@@ -240,6 +244,7 @@ class DecoratedFlex extends StatelessWidget {
     this.center,
     this.sliver = false,
     this.verticalDirection,
+    this.fillRemaining,
     this.children,
   }) : super(key: key);
 
@@ -343,6 +348,9 @@ class DecoratedFlex extends StatelessWidget {
 
   /// 垂直方向
   final VerticalDirection verticalDirection;
+
+  /// 占据剩余的空间
+  final bool fillRemaining;
 
   /// 子元素
   final List<Widget> children;
@@ -483,6 +491,20 @@ class DecoratedFlex extends StatelessWidget {
 
     if (offset != null) {
       result = Transform.translate(offset: offset, child: result);
+    }
+
+    if (fillRemaining == true) {
+      result = LayoutBuilder(
+        builder: (context, constraint) {
+          if (direction == Axis.horizontal) {
+            return SizedBox(width: constraints.maxWidth, child: result);
+          } else if (direction == Axis.vertical) {
+            return SizedBox(height: constraints.maxHeight, child: result);
+          } else {
+            return result;
+          }
+        },
+      );
     }
 
     if (expanded == true) {
