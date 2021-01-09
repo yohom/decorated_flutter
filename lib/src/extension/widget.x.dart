d51f458cf5787fe1ccf784dcbe2628928c36a809
yohom <382146139@qq.com>
2021-01-09 08:49:09 +0800

    enhance: 增加(脱离widget树)直接对Widget进行截图的扩展

--- lib/src/extension/widget.x.dart
+++ lib/src/extension/widget.x.dart
@@ -1,3 +1,7 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 extension WidgetX on Widget {
@@ -8,5 +12,64 @@ extension WidgetX on Widget {
       softWrap: true,
       overflow: TextOverflow.ellipsis,
     );
+  }
+
+  Future<Uint8List> capture({
+    Duration wait,
+    Size logicalSize,
+    Size imageSize,
+  }) async {
+    final RenderRepaintBoundary repaintBoundary = RenderRepaintBoundary();
+
+    logicalSize ??= ui.window.physicalSize / ui.window.devicePixelRatio;
+    imageSize ??= ui.window.physicalSize;
+
+    assert(logicalSize.aspectRatio == imageSize.aspectRatio);
+
+    final RenderView renderView = RenderView(
+      window: null,
+      child: RenderPositionedBox(
+        alignment: Alignment.center,
+        child: repaintBoundary,
+      ),
+      configuration: ViewConfiguration(
+        size: logicalSize,
+        devicePixelRatio: 1.0,
+      ),
+    );
+
+    final PipelineOwner pipelineOwner = PipelineOwner();
+    final BuildOwner buildOwner = BuildOwner();
+
+    pipelineOwner.rootNode = renderView;
+    renderView.prepareInitialFrame();
+
+    final rootElement = RenderObjectToWidgetAdapter<RenderBox>(
+      container: repaintBoundary,
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: this,
+      ),
+    ).attachToRenderTree(buildOwner);
+
+    buildOwner.buildScope(rootElement);
+
+    if (wait != null) {
+      await Future.delayed(wait);
+    }
+
+    buildOwner.buildScope(rootElement);
+    buildOwner.finalizeTree();
+
+    pipelineOwner.flushLayout();
+    pipelineOwner.flushCompositingBits();
+    pipelineOwner.flushPaint();
+
+    final ui.Image image = await repaintBoundary.toImage(
+      pixelRatio: imageSize.width / logicalSize.width,
+    );
+    final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+
+    return byteData.buffer.asUint8List();
   }
 }
