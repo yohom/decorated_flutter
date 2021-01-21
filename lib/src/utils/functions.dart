66ad75a08c697cdd73c025e52b0be7edd9cc9e34
yohom <382146139@qq.com>
2021-01-21 16:54:37 +0800

    enhance: 增加hideKeyboard

--- lib/src/utils/functions.dart
+++ lib/src/utils/functions.dart
@@ -1,4 +1,5 @@
 import 'package:decorated_flutter/decorated_flutter.dart';
+import 'package:flutter/services.dart';
 
 final notEqual = (prev, next) => prev != next;
 final notNull = (data) => data != null;
@@ -9,3 +10,10 @@ final returnNull = () => null;
 final Function doNothing = () {};
 final Function doNothing1 = (_) {};
 final Function doNothing2 = (_, __) {};
+
+/// 关闭键盘
+///
+/// 这个函数和通过FocusScope.unfocus()的区别是在关闭键盘的同时可以保持焦点
+Future<void> hideKeyboard() {
+  return SystemChannels.textInput.invokeMethod('TextInput.hide');
+}
