daecd313daae8078fbfc51a41410e8449df1cd21
yohom <382146139@qq.com>
2021-02-19 13:00:36 +0800

    enhance: 优化日志打印

--- lib/src/extension/file.x.dart
+++ lib/src/extension/file.x.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 
-extension FileX on File {
+extension FileX on FileSystemEntity {
   String get name {
     return path.split(Platform.pathSeparator).last;
+  }
+
+  void deleteIfExists({bool recursive = false}) {
+    if (existsSync()) deleteSync(recursive: recursive);
   }
 }
