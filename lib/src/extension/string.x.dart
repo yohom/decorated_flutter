aafc0a8d24af47021ceaeaad79ab45bf7ee16e35
yohom <382146139@qq.com>
2021-02-20 16:23:26 +0800

    enhance: StringX增加parseJson方法

--- lib/src/extension/string.x.dart
+++ lib/src/extension/string.x.dart
@@ -1,7 +1,14 @@
+import 'dart:convert';
+
 extension StringX on String {
   String plus(String addend) {
     final additionNum = num.parse(addend);
     final thisNum = num.parse(this);
     return '${additionNum + thisNum}';
+  }
+
+  dynamic parseJson() {
+    if (this == null) return null;
+    return jsonDecode(this);
   }
 }
