1e8de1ef5702e5a5867fc7a3e26e175a9a851991
yohom <382146139@qq.com>
2021-02-28 13:10:29 +0800

    enhance: 增加Repeat

---
+++ lib/src/ui/widget/nonvisual/repeat.widget.dart
@@ -0,0 +1,23 @@
+import 'package:decorated_flutter/decorated_flutter.dart';
+import 'package:flutter/material.dart';
+
+class Repeat extends StatelessWidget {
+  const Repeat({
+    Key key,
+    @required this.builder,
+    @required this.interval,
+    this.backgroundColor = Colors.black,
+  }) : super(key: key);
+
+  final WidgetBuilder builder;
+  final Duration interval;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<dynamic>(
+      stream: Stream.periodic(interval, passthrough),
+      builder: (context, _) => builder(context),
+    );
+  }
+}
