c057730a6a008eaf929c7c28c5aef86515e2c0d1
yohom <382146139@qq.com>
2021-03-27 20:27:01 +0800

    enhance: 增加seconds, minutes, hours

--- lib/src/extension/duration.x.dart
+++ lib/src/extension/duration.x.dart
@@ -1,8 +1,5 @@
 extension DurationX on Duration {
   String format({String format = 'HH:mm'}) {
-    final hours = inSeconds ~/ 3600;
-    final minutes = inSeconds % 3600 ~/ 60;
-    final seconds = inSeconds % 3600 ~/ 60 ~/ 60;
     return format
         .replaceAll('HH', hours.toString().padLeft(2, '0'))
         .replaceAll('mm', minutes.toString().padLeft(2, '0'))
@@ -11,4 +8,10 @@ extension DurationX on Duration {
         .replaceAll('m', minutes.toString())
         .replaceAll('s', seconds.toString());
   }
+
+  int get hours => inSeconds ~/ 3600;
+
+  int get minutes => inSeconds % 3600 ~/ 60;
+
+  int get seconds => inSeconds % 3600 ~/ 60 ~/ 60;
 }
