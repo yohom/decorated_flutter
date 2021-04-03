c4b5e9b8673a62de468be2c250610c65c688c170
yohom <382146139@qq.com>
2021-04-03 17:19:50 +0800

    enhance: 调整VisibilityBuilder

--- lib/src/ui/widget/visual/preferred/visibility_builder.widget.dart
+++ lib/src/ui/widget/visual/preferred/visibility_builder.widget.dart
@@ -1,24 +1,23 @@
 import 'package:decorated_flutter/decorated_flutter.dart';
-import 'package:decorated_flutter/src/bloc/bloc.export.dart';
 import 'package:flutter/material.dart';
 
 class VisibilityBuilder extends StatelessWidget {
   const VisibilityBuilder({
     Key key,
     this.initialValue = true,
-    @required this.visibilityIO,
+    @required this.visibilityStream,
     @required this.child,
   }) : super(key: key);
 
   final bool initialValue;
-  final BoolOutput visibilityIO;
+  final Stream<bool> visibilityStream;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return Subscriber<bool>(
       initialData: initialValue,
-      stream: visibilityIO.stream,
+      stream: visibilityStream,
       builder: (data) {
         return Visibility(visible: data, child: child);
       },
