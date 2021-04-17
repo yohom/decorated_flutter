488c350420031ff0c6b5ba01af5aec79c1644fd8
yohom <382146139@qq.com>
2021-04-17 18:29:31 +0800

    enhance: global_key.x.dart迁移nnbd

--- lib/src/extension/global_key.x.dart
+++ lib/src/extension/global_key.x.dart
@@ -1,5 +1,3 @@
-// @dart=2.9
-
 import 'dart:typed_data';
 import 'dart:ui';
 
@@ -8,9 +6,9 @@ import 'package:flutter/rendering.dart';
 
 extension GlobalKeyX on GlobalKey {
   Future<Uint8List> capture() {
-    return (currentContext.findRenderObject() as RenderRepaintBoundary)
-        .toImage(pixelRatio: MediaQuery.of(currentContext).devicePixelRatio)
+    return (currentContext!.findRenderObject() as RenderRepaintBoundary)
+        .toImage(pixelRatio: MediaQuery.of(currentContext!).devicePixelRatio)
         .then((image) => image.toByteData(format: ImageByteFormat.png))
-        .then((byteData) => byteData.buffer.asUint8List());
+        .then((byteData) => byteData!.buffer.asUint8List());
   }
 }
