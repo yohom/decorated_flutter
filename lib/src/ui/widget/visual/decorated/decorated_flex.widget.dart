f0afda8d9788041d6b6efa22654195d85ffa2897
yohom <382146139@qq.com>
2021-04-17 23:41:20 +0800

    fix: divider修正

--- lib/src/ui/widget/visual/decorated/decorated_flex.widget.dart
+++ lib/src/ui/widget/visual/decorated/decorated_flex.widget.dart
@@ -374,7 +374,7 @@ class DecoratedFlex extends StatelessWidget {
       textBaseline: textBaseline,
       verticalDirection: verticalDirection ?? VerticalDirection.down,
       children: itemSpacing != 0 || divider != null
-          ? addItemDivider(_children, itemSpacing!, divider!)
+          ? addItemDivider(_children, itemSpacing!, divider)
           : _children,
     );
 
