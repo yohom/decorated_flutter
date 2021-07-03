d361980c7afce66b2d30264a40dec36118106e0b
yohom <382146139@qq.com>
2021-07-03 09:21:56 +0800

    enhance: 增加LazyIndexedStack

---
+++ lib/src/ui/widget/visual/decorated/lazy_indexed_stack.widget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/widgets.dart';
+
+class LazyIndexedStack extends StatefulWidget {
+  LazyIndexedStack({
+    Key? key,
+    this.alignment = AlignmentDirectional.topStart,
+    this.textDirection,
+    this.sizing = StackFit.loose,
+    required this.index,
+    this.reuse = true,
+    required this.itemBuilder,
+    this.itemCount = 0,
+  }) : super(key: key);
+
+  final AlignmentGeometry alignment;
+  final TextDirection? textDirection;
+  final StackFit sizing;
+  final int index;
+  final bool reuse;
+  final int itemCount;
+  final IndexedWidgetBuilder itemBuilder;
+
+  @override
+  _LazyIndexedStackState createState() => _LazyIndexedStackState();
+}
+
+class _LazyIndexedStackState extends State<LazyIndexedStack> {
+  late List<Widget> _children;
+  late List<bool> _loaded;
+
+  @override
+  void initState() {
+    super.initState();
+    _loaded = [];
+    _children = [];
+    for (int i = 0; i < widget.itemCount; ++i) {
+      if (i == widget.index) {
+        _children.add(widget.itemBuilder(context, i));
+        _loaded.add(true);
+      } else {
+        _children.add(Container());
+        _loaded.add(false);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IndexedStack(
+      index: widget.index,
+      alignment: widget.alignment,
+      textDirection: widget.textDirection,
+      sizing: widget.sizing,
+      children: _children,
+    );
+  }
+
+  @override
+  void didUpdateWidget(LazyIndexedStack oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    for (int i = 0; i < widget.itemCount; ++i) {
+      if (i == widget.index) {
+        if (!_loaded[i]) {
+          _children[i] = widget.itemBuilder(context, i);
+          _loaded[i] = true;
+        } else {
+          if (widget.reuse) {
+            return;
+          }
+          _children[i] = widget.itemBuilder(context, i);
+        }
+      }
+    }
+  }
+}
