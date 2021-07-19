1a037c8a33e204b0fba0133876b39c751ccbd4d8
yohom <382146139@qq.com>
2021-07-19 11:19:36 +0800

    test: 单元测试

--- test/bloc/bloc_io/optional_test.dart
+++ test/bloc/bloc_io/optional_test.dart
@@ -13,4 +13,30 @@ void main() {
       expect(static.get() == null, true);
     });
   });
+
+  group('OptionalInput', () {
+    test('add值等于latest的值', () {
+      final input = OptionalInput<String>(semantics: '测试');
+      expect(input.latest == null, true);
+
+      input.add('abc');
+      expect(input.latest == 'abc', true);
+    });
+
+    test('acceptEmpty为FALSE时, 不接受被判定为空的值', () {
+      // TODO
+    });
+
+    test('isDistinct为true时, 不接受被判定为相等的值', () {
+      // TODO
+    });
+
+    test('设置了onReset时, 调用reset方法, latest应该是onReset的返回值', () {
+      // TODO
+    });
+
+    test('设置了persistentKey时, 应该调用内部持久层的write方法', () {
+      // TODO
+    });
+  });
 }
