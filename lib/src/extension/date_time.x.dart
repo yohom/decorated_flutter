3cba363eca3afc6e2498e3dcaee4b61c5c15c2ae
yohom <yohombao@qq.com>
2021-08-12 23:32:22 +0800

    enhance: 增加copyWith

--- lib/src/extension/date_time.x.dart
+++ lib/src/extension/date_time.x.dart
@@ -35,4 +35,26 @@ extension DateTimeX on DateTime {
     final today = DateTime(now.year, now.month, now.day + 1);
     return DateTime(year, month, day) == today;
   }
+
+  DateTime copyWith({
+    int? year,
+    int? month,
+    int? day,
+    int? hour,
+    int? minute,
+    int? second,
+    int? millisecond,
+    int? microsecond,
+  }) {
+    return DateTime(
+      year ?? this.year,
+      month ?? this.month,
+      day ?? this.day,
+      hour ?? this.hour,
+      minute ?? this.minute,
+      second ?? this.second,
+      millisecond ?? this.millisecond,
+      microsecond ?? this.microsecond,
+    );
+  }
 }
