31139f5976525c682ddde74973b599082db44d47
yohom <yohombao@qq.com>
2021-11-13 21:53:41 +0800

    enhance: 整理

--- lib/src/extension/text_editing_controller.x.dart
+++ lib/src/extension/text_editing_controller.x.dart
@@ -6,13 +6,12 @@ import 'package:flutter/material.dart';
 extension TextEditingControllerX on TextEditingController {
   void append(String text) {
     this.text = this.text + text;
-    selection =
-        TextSelection.fromPosition(TextPosition(offset: this.text.length));
+    selection = TextSelection.collapsed(offset: this.text.length);
   }
 
   void backspace([int count = 1]) {
     final endIndex = max(0, text.length - count);
     text = text.substring(0, endIndex);
-    selection = TextSelection.fromPosition(TextPosition(offset: text.length));
+    selection = TextSelection.collapsed(offset: text.length);
   }
 }
