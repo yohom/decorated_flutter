331c1e35a91926f618453544278a77b6f5663bbf
yohom <yohombao@qq.com>
2021-12-09 21:22:12 +0800

    Revert "enhance: 简化BLoCProvider"
    This reverts commit 1484f4c0bc6d3ab4822b45ccc15ed878d476eb0a.

--- lib/src/bloc/bloc_provider.widget.dart
+++ lib/src/bloc/bloc_provider.widget.dart
@@ -2,6 +2,8 @@ import 'package:decorated_flutter/decorated_flutter.dart';
 import 'package:decorated_flutter/src/bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
+typedef _Init<T extends BLoC> = void Function(T bloc);
+
 class BLoCProvider<T extends BLoC> extends StatefulWidget {
   const BLoCProvider({
     Key? key,
@@ -13,7 +15,7 @@ class BLoCProvider<T extends BLoC> extends StatefulWidget {
   }) : super(key: key);
 
   final T bloc;
-  final ValueChanged<T>? init;
+  final _Init<T>? init;
   final Widget child;
   final VoidCallback? onDispose;
   final bool autoDispose;
@@ -22,8 +24,9 @@ class BLoCProvider<T extends BLoC> extends StatefulWidget {
   _BLoCProviderState<T> createState() => _BLoCProviderState<T>();
 
   static T? of<T extends BLoC>(BuildContext context) {
-    final provider =
-        context.findAncestorStateOfType<_BLoCProviderState<T>>()?.widget;
+    _BLoCProviderInherited<T>? provider = context
+        .getElementForInheritedWidgetOfExactType<_BLoCProviderInherited<T>>()
+        ?.widget as _BLoCProviderInherited<T>?;
     return provider?.bloc;
   }
 }
@@ -32,11 +35,17 @@ class _BLoCProviderState<T extends BLoC> extends State<BLoCProvider<T>> {
   @override
   void initState() {
     super.initState();
-    if (widget.init != null) widget.init!(widget.bloc);
+
+    if (isNotEmpty(widget.init)) widget.init!(widget.bloc);
   }
 
   @override
-  Widget build(BuildContext context) => widget.child;
+  Widget build(BuildContext context) {
+    return _BLoCProviderInherited(
+      child: widget.child,
+      bloc: widget.bloc,
+    );
+  }
 
   @override
   void dispose() {
@@ -49,4 +58,17 @@ class _BLoCProviderState<T extends BLoC> extends State<BLoCProvider<T>> {
     }
     super.dispose();
   }
+}
+
+class _BLoCProviderInherited<T> extends InheritedWidget {
+  const _BLoCProviderInherited({
+    Key? key,
+    required Widget child,
+    required this.bloc,
+  }) : super(key: key, child: child);
+
+  final T bloc;
+
+  @override
+  bool updateShouldNotify(_BLoCProviderInherited oldWidget) => false;
 }
