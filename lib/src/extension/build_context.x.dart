9a04c0b7198d9cd4be35837e6cf406264d2d93d1
yohom <yohombao@qq.com>
2021-12-19 13:33:38 +0800

    enhance: 补完BlurDialog参数

--- lib/src/extension/build_context.x.dart
+++ lib/src/extension/build_context.x.dart
@@ -1,9 +1,5 @@
-import 'dart:ui';
-
 import 'package:decorated_flutter/src/bloc/bloc.export.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
 extension BuildContextX on BuildContext {
   T of<T extends BLoC>() {
