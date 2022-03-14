6f3bb5c9eddbf7f3095da8f7faf2d2fc1990ec0f
yohom <yohombao@qq.com>
2022-03-14 08:48:06 +0800

    enhance: 增加anyString

--- lib/src/utils/objects.dart
+++ lib/src/utils/objects.dart
@@ -3,6 +3,8 @@ import 'package:flutter/widgets.dart';
 /// 当不关心对象是什么, 但是又必须存在一个对象的时候, 使用这个
 final anyObject = Object();
 
+const anyString = '';
+
 final gNavigatorKey = GlobalKey<NavigatorState>();
 
 final kMobileRegex =
