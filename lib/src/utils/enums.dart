e1a7d1833bf134fed08f235f738211c981f93626
yohom <yohombao@qq.com>
2022-04-25 14:30:18 +0800

    enhance: 增加增加星座相关

--- lib/src/utils/enums.dart
+++ lib/src/utils/enums.dart
@@ -21,3 +21,19 @@ enum Gender {
   female,
   unknown,
 }
+
+enum Constellation {
+  capricorn, // Capricorn 摩羯座（12月22日～1月20日）
+  aquarius, // Aquarius 水瓶座（1月21日～2月19日）
+  pisces, // Pisces 双鱼座（2月20日～3月20日）
+  aries, // 3月21日～4月20日
+  taurus, // 4月21～5月21日
+  gemini, // 5月22日～6月21日
+  cancer, // Cancer 巨蟹座（6月22日～7月22日）
+  leo, // Leo 狮子座（7月23日～8月23日）
+  virgo, // Virgo 处女座（8月24日～9月23日）
+  libra, // Libra 天秤座（9月24日～10月23日）
+  scorpio, // Scorpio 天蝎座（10月24日～11月22日）
+  sagittarius, // Sagittarius 射手座（11月23日～12月21日）
+  unknown, // 未知
+}
