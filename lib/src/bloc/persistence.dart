dbde564212227311e6be125091f94b836d68c60e
yohom <yohombao@qq.com>
2022-05-06 10:49:36 +0800

    enhance: [breaking change] 增加持久化key重复的assert

--- lib/src/bloc/persistence.dart
+++ lib/src/bloc/persistence.dart
@@ -9,6 +9,8 @@ abstract class Persistence {
 }
 
 class PersistConfig<T> {
+  static final Set<String> _keySet = {};
+
   /// 序列化唯一标识
   final String key;
 
@@ -22,5 +24,8 @@ class PersistConfig<T> {
     required this.key,
     required this.onDeserialize,
     required this.onSerialize,
-  });
+  }) {
+    assert(!_keySet.contains(key), '和已有的持久化key重复, 请另选key!');
+    _keySet.add(key);
+  }
 }
