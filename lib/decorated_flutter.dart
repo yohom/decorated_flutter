731e7c29a968affd091d6ee3847fdefa71b2079e
yohom <yohombao@qq.com>
2022-06-10 15:14:34 +0800

    enhance: 导出intl

--- lib/decorated_flutter.dart
+++ lib/decorated_flutter.dart
@@ -1,5 +1,6 @@
 library decorated_flutter;
 
+export 'package:intl/intl.dart';
 export 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 export 'package:path_provider/path_provider.dart';
 export 'package:rxdart/rxdart.dart';
@@ -7,6 +8,7 @@ export 'package:sliver_tools/sliver_tools.dart';
 
 export 'src/annotation/annotation.export.dart';
 export 'src/bloc/bloc.export.dart';
+export 'src/experimental/experimental.export.dart';
 export 'src/extension/extension.export.dart';
 export 'src/misc/misc.export.dart';
 export 'src/mixin/mixin.export.dart';
@@ -14,4 +16,3 @@ export 'src/model/model.export.dart';
 export 'src/res/res.export.dart';
 export 'src/ui/ui.export.dart';
 export 'src/utils/utils.export.dart';
-export 'src/experimental/experimental.export.dart';
