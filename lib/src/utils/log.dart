49cfc53e00bda302b8099623dcdec0f61393811e
yohom <yohombao@qq.com>
2022-06-24 17:22:02 +0800

    enhance: 整理

--- lib/src/utils/log.dart
+++ lib/src/utils/log.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
-import 'package:logger/src/outputs/file_output.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -36,9 +35,6 @@ class _Logger {
       _logger = Logger(
         printer: PrettyPrinter(printTime: true),
         filter: kDebugMode ? DevelopmentFilter() : InfoFilter(),
-        output: MultiOutput(
-          [ConsoleOutput(), if (!kIsWeb) FileOutput(file: logFile)],
-        ),
       );
     }
   }
