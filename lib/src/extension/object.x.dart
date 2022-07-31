e457c123d24bee6280b73f020087322e0a1d1d6d
yohom <yohombao@qq.com>
2022-07-31 11:03:17 +0800

    enhance: 精简

--- lib/src/extension/object.x.dart
+++ lib/src/extension/object.x.dart
@@ -8,19 +8,19 @@ extension KtStandardExtension<T> on T {
   R let<R>(R Function(T self) block) {
     return block(this);
   }
-
-  T apply(void Function() block) {
-    block();
-    return this;
-  }
-
-  R also<R>(R Function(T self) func) {
-    return func(this);
-  }
-
-  R run<R>(R Function() func) {
-    return func();
-  }
+  //
+  // T apply(void Function() block) {
+  //   block();
+  //   return this;
+  // }
+  //
+  // R also<R>(R Function(T self) func) {
+  //   return func(this);
+  // }
+  //
+  // R run<R>(R Function() func) {
+  //   return func();
+  // }
 
   T? takeIf(bool Function(T self) predicate) {
     if (predicate(this)) {
