13c28094b50b25d1f6fb835a6a8037d1a4302e45
yohom <yohombao@qq.com>
2022-10-17 15:18:37 +0800

    enhance: 优化anyObject

--- lib/src/utils/objects.dart
+++ lib/src/utils/objects.dart
@@ -4,7 +4,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'log.dart';
 
 /// 当不关心对象是什么, 但是又必须存在一个对象的时候, 使用这个
-final anyObject = Object();
+final dynamic anyObject = Object();
 
 const anyString = '';
 
