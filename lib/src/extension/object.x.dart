8994549f3a073b147db61b6b3abcb1eab0d77408
yohom <yohombao@qq.com>
2022-10-20 11:10:48 +0800

    enhance: 增加list

--- lib/src/extension/object.x.dart
+++ lib/src/extension/object.x.dart
@@ -2,6 +2,21 @@ import 'dart:developer' as devtools show log;
 
 extension ObjectX on Object {
   void log([String? prefix]) => devtools.log('$prefix${toString()}');
+
+  List<T> list<T>() {
+    return (this as List).cast<T>();
+  }
+
+  Map<K, V> map<K, V>() {
+    return (this as Map).cast<K, V>();
+  }
+
+  List<Map<K, V>> listMap<K, V>({bool growable = false}) {
+    return (this as List)
+        .cast<Map>()
+        .map((e) => e.cast<K, V>())
+        .toList(growable: growable);
+  }
 }
 
 extension KtStandardExtension<T> on T {
