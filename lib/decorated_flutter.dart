670d7a6523688f7aba05a72f0aad660c4bfcea5b
yohom <yohombao@qq.com>
2022-11-07 13:36:01 +0800

    enhance: 去掉不常用的cache_manager插件, 需要的时候再单独引用

--- lib/decorated_flutter.dart
+++ lib/decorated_flutter.dart
@@ -8,7 +8,6 @@ export 'package:oktoast/oktoast.dart';
 export 'package:package_info_plus/package_info_plus.dart';
 export 'package:path_provider/path_provider.dart';
 export 'package:rxdart/rxdart.dart';
-export 'package:sliver_tools/sliver_tools.dart';
 
 export 'src/annotation/annotation.export.dart';
 export 'src/bloc/bloc.export.dart';
