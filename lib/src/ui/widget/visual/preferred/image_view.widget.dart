fc21774dc85c0f9cf5fec69576ad556656191be7
yohom <yohombao@qq.com>
2023-01-10 10:42:26 +0800

    enhance: 删除图片加解密的逻辑

--- lib/src/ui/widget/visual/preferred/image_view.widget.dart
+++ lib/src/ui/widget/visual/preferred/image_view.widget.dart
@@ -1,19 +1,17 @@
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:decorated_flutter/src/annotation/annotation.export.dart';
 import 'package:decorated_flutter/src/extension/extension.export.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../../../utils/utils.export.dart';
 
 typedef LoadingProgress = void Function(double progress, List<int> data);
 
+@Deprecated('暂时无用')
 class CryptoOption {
   /// 解密
   final Future<Uint8List> Function(Uint8List encrypted) decrypt;
@@ -44,11 +42,13 @@ class CryptoOption {
 
 class ImageView extends StatelessWidget {
   /// 加解密选项
+  @Deprecated('暂时无用')
   static CryptoOption? _cryptoOption;
   static Widget? globalErrorWidget;
   static Widget? globalPlaceholder;
   static bool logEnable = false;
 
+  @Deprecated('暂时无用')
   static set cryptoOption(CryptoOption? value) {
     _cryptoOption = value;
   }
@@ -346,25 +346,25 @@ class ImageView extends StatelessWidget {
         );
         if (logEnable) L.d('使用SvgPicture.network: $imageUrl');
       } else {
-        if (ImageView._cryptoOption?.enableNetworkImage == true) {
-          result = _encryptedImage(context, imageUrl!);
-          if (logEnable) L.d('使用_encryptedImage: $imageUrl');
-        } else {
-          result = CachedNetworkImage(
-            imageUrl: imageUrl!,
-            key: autoApplyKey ? Key(imageUrl!) : null,
-            width: width,
-            height: height,
-            fit: fit,
-            color: _color,
-            placeholder: _placeholder != null ? (_, __) => _placeholder : null,
-            errorWidget:
-                _errorWidget != null ? (_, __, ___) => _errorWidget : null,
-            memCacheWidth: _cacheWidth,
-            memCacheHeight: _cacheHeight,
-          );
-          if (logEnable) L.d('使用CachedNetworkImage: $imageUrl');
-        }
+        // if (ImageView._cryptoOption?.enableNetworkImage == true) {
+        // result = _encryptedImage(context, imageUrl!);
+        // if (logEnable) L.d('使用_encryptedImage: $imageUrl');
+        // } else {
+        result = CachedNetworkImage(
+          imageUrl: imageUrl!,
+          key: autoApplyKey ? Key(imageUrl!) : null,
+          width: width,
+          height: height,
+          fit: fit,
+          color: _color,
+          placeholder: _placeholder != null ? (_, __) => _placeholder : null,
+          errorWidget:
+              _errorWidget != null ? (_, __, ___) => _errorWidget : null,
+          memCacheWidth: _cacheWidth,
+          memCacheHeight: _cacheHeight,
+        );
+        if (logEnable) L.d('使用CachedNetworkImage: $imageUrl');
+        // }
       }
     } else {
       // 如果图片地址为null的话, 那就不显示
@@ -406,52 +406,52 @@ class ImageView extends StatelessWidget {
     return result;
   }
 
-  Widget _encryptedImage(BuildContext context, String imageUri) {
-    final isDarkMode = context.isDarkMode;
-    Color? _color = color;
-    if (autoDarkMode) {
-      _color = isDarkMode ? Colors.white : null;
-    }
-    final _width = size ?? width;
-    final _height = size ?? height;
-    final _cacheWidth = cacheSize ?? cacheWidth;
-    final _cacheHeight = cacheSize ?? cacheHeight;
-
-    Future<Uint8List> imageFuture;
-    if (kIsWeb) {
-      // web端就先不缓存了
-      imageFuture = Dio()
-          .get(imageUri, options: Options(responseType: ResponseType.bytes))
-          .then((value) => value.data);
-    } else {
-      imageFuture = DefaultCacheManager()
-          .getSingleFile(imageUri)
-          .then((value) => value.readAsBytes());
-    }
-
-    return FutureBuilder<Uint8List>(
-      future: imageFuture.then(ImageView._cryptoOption!.decrypt),
-      builder: (_, snapshot) {
-        if (snapshot.hasData) {
-          return Image.memory(
-            snapshot.requireData,
-            key: autoApplyKey ? Key(imageUri) : null,
-            width: _width,
-            height: _height,
-            cacheWidth: _cacheWidth,
-            cacheHeight: _cacheHeight,
-            color: _color,
-            fit: fit,
-            gaplessPlayback: true,
-          );
-        } else if (snapshot.hasError) {
-          return errorWidget ?? globalErrorWidget ?? const SizedBox.shrink();
-        } else {
-          return placeholder ?? globalPlaceholder ?? const SizedBox.shrink();
-        }
-      },
-    );
-  }
+  // Widget _encryptedImage(BuildContext context, String imageUri) {
+  //   final isDarkMode = context.isDarkMode;
+  //   Color? _color = color;
+  //   if (autoDarkMode) {
+  //     _color = isDarkMode ? Colors.white : null;
+  //   }
+  //   final _width = size ?? width;
+  //   final _height = size ?? height;
+  //   final _cacheWidth = cacheSize ?? cacheWidth;
+  //   final _cacheHeight = cacheSize ?? cacheHeight;
+  //
+  //   Future<Uint8List> imageFuture;
+  //   if (kIsWeb) {
+  //     // web端就先不缓存了
+  //     imageFuture = Dio()
+  //         .get(imageUri, options: Options(responseType: ResponseType.bytes))
+  //         .then((value) => value.data);
+  //   } else {
+  //     imageFuture = DefaultCacheManager()
+  //         .getSingleFile(imageUri)
+  //         .then((value) => value.readAsBytes());
+  //   }
+  //
+  //   return FutureBuilder<Uint8List>(
+  //     future: imageFuture.then(ImageView._cryptoOption!.decrypt),
+  //     builder: (_, snapshot) {
+  //       if (snapshot.hasData) {
+  //         return Image.memory(
+  //           snapshot.requireData,
+  //           key: autoApplyKey ? Key(imageUri) : null,
+  //           width: _width,
+  //           height: _height,
+  //           cacheWidth: _cacheWidth,
+  //           cacheHeight: _cacheHeight,
+  //           color: _color,
+  //           fit: fit,
+  //           gaplessPlayback: true,
+  //         );
+  //       } else if (snapshot.hasError) {
+  //         return errorWidget ?? globalErrorWidget ?? const SizedBox.shrink();
+  //       } else {
+  //         return placeholder ?? globalPlaceholder ?? const SizedBox.shrink();
+  //       }
+  //     },
+  //   );
+  // }
 }
 
 extension on String {
