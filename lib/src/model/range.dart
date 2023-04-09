c6e2c8102e20826944f541e3380a2370b6a79eff
yohom <yohombao@qq.com>
2023-04-09 21:51:10 +0800

    enhance: 增加contains

--- lib/src/model/range.dart
+++ lib/src/model/range.dart
@@ -4,6 +4,24 @@ class Range<T extends num> {
 
   const Range(this.lower, this.upper);
 
+  bool contains(
+    num number, {
+    bool lowerInclusive = true,
+    bool upperInclusive = false,
+  }) {
+    if (lowerInclusive && (number < lower)) {
+      return false;
+    } else if (!lowerInclusive && (number <= lower)) {
+      return false;
+    } else if (upperInclusive && (number > upper)) {
+      return false;
+    } else if (!upperInclusive && (number >= upper)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   String format([String format = 'l-u']) {
     return format.replaceAll('l', '$lower').replaceAll('u', '$upper');
   }
