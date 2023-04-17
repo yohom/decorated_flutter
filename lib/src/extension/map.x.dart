cdb93f35f55011a64d2a59d6b65f49604477490a
yohom <yohombao@qq.com>
2023-04-17 18:03:36 +0800

    enhance: valueFor支持列表获取

--- lib/src/extension/map.x.dart
+++ lib/src/extension/map.x.dart
@@ -1,3 +1,5 @@
+import 'package:decorated_flutter/decorated_flutter.dart';
+
 extension MapX<K, V> on Map<K, V> {
   Object? valueFor({required String keyPath}) {
     final keysSplit = keyPath.split('.');
@@ -7,8 +9,22 @@ extension MapX<K, V> on Map<K, V> {
       return thisValue;
     } else if (thisValue is Map) {
       return thisValue.valueFor(keyPath: keysSplit.join('.'));
+    } else if (thisValue is List) {
+      final nextKey = keysSplit.removeAt(0);
+      if (nextKey.intValue != null) {
+        final listIndex = nextKey.intValue!;
+        final value = thisValue[listIndex];
+        if (value is Map) {
+          value.valueFor(keyPath: keysSplit.join('.'));
+        } else {
+          return value;
+        }
+      } else {
+        return null;
+      }
     } else {
       return null;
     }
+    return null;
   }
 }
