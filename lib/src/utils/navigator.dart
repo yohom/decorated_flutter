85e3a57cb4baba842fb494983709eb6a32313f0a
yohom <yohombao@qq.com>
2023-04-23 17:46:45 +0800

    chore: 增加注释

--- lib/src/utils/navigator.dart
+++ lib/src/utils/navigator.dart
@@ -20,6 +20,9 @@ Future<T?> pushRoute<T>(
 }
 
 /// 替换一个路由
+///
+/// [query]是一个只有字符串键值对的Map, 直接拼接在路径后面, 这样可以在web环境下保持状态
+/// [body]可以为任意对象, 但是不能在不同会话之间保持(比如web端的刷新)
 Future<T?> replaceRoute<T extends Object?, TO extends Object?>(
   String path, {
   Map<String, String?>? query,
@@ -34,6 +37,9 @@ Future<T?> replaceRoute<T extends Object?, TO extends Object?>(
 }
 
 /// push一个路由并弹出指定路由
+///
+/// [query]是一个只有字符串键值对的Map, 直接拼接在路径后面, 这样可以在web环境下保持状态
+/// [body]可以为任意对象, 但是不能在不同会话之间保持(比如web端的刷新)
 Future<T?> pushRemoveUntil<T extends Object?, TO extends Object?>(
   String path,
   RoutePredicate predicate, {
