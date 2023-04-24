639f9779206dadebfe599821355aad04d5b1afce
yohom <yohombao@qq.com>
2023-04-24 10:10:32 +0800

    enhance: 优化capture方法

--- lib/src/extension/global_key.x.dart
+++ lib/src/extension/global_key.x.dart
@@ -5,11 +5,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 extension GlobalKeyX on GlobalKey {
-  Future<Uint8List?> capture() {
-    return (currentContext?.findRenderObject() as RenderRepaintBoundary?)
-            ?.toImage(pixelRatio: window.devicePixelRatio)
-            .then((image) => image.toByteData(format: ImageByteFormat.png))
-            .then((byteData) => byteData!.buffer.asUint8List()) ??
-        Future.value();
+  Future<Uint8List?> capture() async {
+    final box = (currentContext?.findRenderObject() as RenderRepaintBoundary?);
+    if (box == null) return null;
+
+    int attempts = 0;
+    while (box.debugNeedsPaint && attempts < 10) {
+      await Future.delayed(const Duration(milliseconds: 16));
+      attempts++;
+    }
+
+    return box
+        .toImage(pixelRatio: window.devicePixelRatio)
+        .then((image) => image.toByteData(format: ImageByteFormat.png))
+        .then((byteData) => byteData!.buffer.asUint8List());
   }
 }
