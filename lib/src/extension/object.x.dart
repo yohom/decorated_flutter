19312b15ac3c7ff07c435f345b24d1d2ba74e181
yohom <yohombao@qq.com>
2023-05-22 15:55:10 +0800

    chore: 整理

--- lib/src/extension/object.x.dart
+++ lib/src/extension/object.x.dart
@@ -3,14 +3,17 @@ import 'dart:developer' as devtools show log;
 extension ObjectX on Object {
   void log([String? prefix]) => devtools.log('$prefix${toString()}');
 
+  @Deprecated('没什么用')
   List<T> list<T>() {
     return (this as List).cast<T>();
   }
 
+  @Deprecated('没什么用')
   Map<K, V> map<K, V>() {
     return (this as Map).cast<K, V>();
   }
 
+  @Deprecated('没什么用')
   List<Map<K, V>> listMap<K, V>({bool growable = false}) {
     return (this as List)
         .cast<Map>()
