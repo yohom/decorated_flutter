aa1b03d7781cbdfbabb856ce7de47d7119500051
yohom <yohombao@qq.com>
2023-06-12 10:47:49 +0800

    enhance: 增加LoadingElevatedButton, 但是感觉不好用, 还不如在app里自行封装

--- lib/src/ui/widget/visual/decorated/buttons.widget.dart
+++ lib/src/ui/widget/visual/decorated/buttons.widget.dart
@@ -1,4 +1,5 @@
 import 'package:decorated_flutter/src/utils/utils.export.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// 带有缩放动画的按钮
@@ -74,6 +75,60 @@ class _AnimatedScaleButtonState extends State<AnimatedScaleButton> {
         },
         child: widget.child,
       ),
+    );
+  }
+}
+
+/// 带加载中动画的按钮
+///
+/// 其中回调是返回Future的函数, 按钮会在回调执行完毕后, 恢复到原状
+@Deprecated('感觉不好用')
+class LoadingElevatedButton extends StatefulWidget {
+  const LoadingElevatedButton({
+    super.key,
+    this.loadingChild = const CupertinoActivityIndicator(),
+    required this.child,
+    this.errorChild,
+    required this.onPressed,
+    this.style,
+  });
+
+  final Widget child;
+  final Widget? loadingChild, errorChild;
+  final ButtonStyle? style;
+  final Future<void> Function() onPressed;
+
+  @override
+  State<LoadingElevatedButton> createState() => _LoadingElevatedButtonState();
+}
+
+class _LoadingElevatedButtonState extends State<LoadingElevatedButton> {
+  late bool _isLoading;
+  late bool _isError;
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = false;
+    _isError = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: widget.style,
+      onPressed: () {
+        setState(() => _isLoading = true);
+        widget
+            .onPressed()
+            .catchError((_) => setState(() => _isError = true))
+            .whenComplete(() => setState(() => _isLoading = false));
+      },
+      child: _isError && widget.errorChild != null
+          ? widget.errorChild!
+          : _isLoading
+              ? widget.loadingChild
+              : widget.child,
     );
   }
 }
