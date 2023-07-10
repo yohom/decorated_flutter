8c06b4c48702994825e05222c57ecf5bc5a246dc
yohom <yohombao@qq.com>
2023-07-10 13:34:45 +0800

    enhance: 增加重试机制

--- lib/src/utils/functions.dart
+++ lib/src/utils/functions.dart
@@ -12,6 +12,7 @@ import 'package:uuid/uuid.dart';
 
 import 'logger/logger.dart';
 import 'objects.dart';
+import 'retry.dart';
 
 bool notEqual(prev, next) => prev != next;
 
@@ -315,4 +316,22 @@ T getFirst<T>(List<T> list) {
 
 T? getFirstOrNull<T>(List<T> list) {
   return list.firstOrNull;
+}
+
+/// 等待某个操作符合要求
+///
+/// 使用[retry]的变体来简化实现
+Future<void> waitFor(
+  FutureOr<bool> Function() fn, {
+  Duration delayFactor = const Duration(milliseconds: 200),
+  double randomizationFactor = 0.25,
+  Duration maxDelay = const Duration(seconds: 30),
+  int maxAttempts = 8,
+}) {
+  return RetryOptions(
+    delayFactor: delayFactor,
+    randomizationFactor: randomizationFactor,
+    maxDelay: maxDelay,
+    maxAttempts: maxAttempts,
+  ).retry(doNothing, retryIf: (_) async => !(await fn()));
 }
