d41577f217bb2be72cee0c094a6bf97aeedfd8a0
yohom <yohombao@qq.com>
2024-01-11 10:39:12 +0800

    enhance: 增加SizeX的一些了扩展方法

--- lib/src/extension/size.x.dart
+++ lib/src/extension/size.x.dart
@@ -8,4 +8,16 @@ extension SizeX on Size {
   bool get isPortrait {
     return aspectRatio <= 1;
   }
+
+  Size floor() {
+    return Size(width.floor().toDouble(), height.floor().toDouble());
+  }
+
+  Size ceil() {
+    return Size(width.ceil().toDouble(), height.ceil().toDouble());
+  }
+
+  Size round() {
+    return Size(width.round().toDouble(), height.round().toDouble());
+  }
 }
