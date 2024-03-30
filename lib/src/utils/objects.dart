0141e38b68dfd332a935613a95887ebe044219e0
yohom <yohombao@qq.com>
2024-03-30 23:34:07 +0800

    Revert "Revert "enhance: initDecoratedBox在finally中初始化box""
    This reverts commit 627a157847468c9ff1910cd45f9bffb3146d9b8d.

--- lib/src/utils/objects.dart
+++ lib/src/utils/objects.dart
@@ -22,8 +22,11 @@ final kIpV4Regex = RegExp(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$');
 late Box _box;
 Future<void> initDecoratedBox() async {
   L.d('开始初始化decorated专属存储持久层');
-  await Hive.initFlutter();
-  _box = await Hive.openBox('decorated_flutter_box');
+  try {
+    await Hive.initFlutter();
+  } finally {
+    _box = await Hive.openBox('decorated_flutter_box');
+  }
   L.d('结束初始化decorated专属存储持久层');
 }
 
