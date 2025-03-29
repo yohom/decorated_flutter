2b4335a1c48d39f69ef666272d3f5229e1e1467a
yohom <yohombao@qq.com>
2025-03-29 11:50:56 +0800

    enhance: 增加BackGestureDetector

---
+++ lib/src/ui/widget/visual/special_affect/back_gesture_detector.widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class BackGestureDetector extends StatelessWidget {
+  const BackGestureDetector({
+    super.key,
+    this.enabled = true,
+    required this.child,
+    this.onBack,
+    this.velocityThreshold = 500,
+  });
+
+  /// 是否开启
+  final bool enabled;
+
+  /// 返回的回调
+  final VoidCallback? onBack;
+
+  /// 触发返回的速度阈值 单位 pixelsPerSecond.dx
+  final int velocityThreshold;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        child,
+        if (enabled)
+          Positioned(
+            left: 0,
+            top: 0,
+            bottom: 0,
+            width: 64,
+            child: GestureDetector(
+              onPanEnd: (details) {
+                if (details.velocity.pixelsPerSecond.dx > velocityThreshold) {
+                  if (onBack != null) {
+                    onBack!();
+                  } else {
+                    Navigator.of(context).pop();
+                  }
+                }
+              },
+            ),
+          ),
+      ],
+    );
+  }
+}
