fb0a8d7e1a8f3738e251f482416ab5710bb7f266
yohom <yohombao@qq.com>
2025-04-02 12:28:00 +0800

    enhance: 整理

--- lib/src/utils/interceptor.dart
+++ lib/src/utils/interceptor.dart
@@ -2,12 +2,12 @@ import 'dart:async';
 
 import 'package:dio/dio.dart';
 
-class DebounceInterceptor extends Interceptor {
-  final Map<String, Timer> _requestTimers = {}; // 记录请求的防抖状态
-  final Duration debounceDuration; // 防抖间隔（毫秒）
+class ThrottleInterceptor extends Interceptor {
+  final Map<String, Timer> _requestTimers = {}; // 记录请求的节流状态
+  final Duration duration; // 防抖间隔
 
-  DebounceInterceptor({
-    this.debounceDuration = const Duration(milliseconds: 256),
+  ThrottleInterceptor({
+    this.duration = const Duration(milliseconds: 256),
   });
 
   @override
@@ -20,14 +20,14 @@ class DebounceInterceptor extends Interceptor {
         DioException(
           requestOptions: options,
           type: DioExceptionType.cancel,
-          error: 'Debounced request: $key',
+          error: 'Throttle Request: $key',
         ),
       );
       return;
     }
 
     // 如果没有重复请求，则允许请求并设置防抖计时器
-    _requestTimers[key] = Timer(debounceDuration, () {
+    _requestTimers[key] = Timer(duration, () {
       _requestTimers.remove(key); // 防抖时间过后移除记录
     });
 
@@ -50,6 +50,6 @@ class DebounceInterceptor extends Interceptor {
 
   /// 根据请求的 URL 和参数生成唯一标识
   String _generateRequestKey(RequestOptions options) {
-    return '${options.method}-${options.uri.toString()}';
+    return '${options.method}-${options.uri.toString()}-${options.data.toString()}';
   }
 }
