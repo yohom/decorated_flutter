40332538d84faa99e0294c1b19b4ff7d1277757b
yohom <yohombao@qq.com>
2025-04-25 09:14:56 +0800

    enhance: 增加稳定性

--- lib/src/utils/logger/io_logger.dart
+++ lib/src/utils/logger/io_logger.dart
@@ -15,11 +15,15 @@ class Logger extends ILogger {
   );
 
   String get _context {
-    final stackTrace = StackTrace.current;
-    final callerFrame = stackTrace.toString().split('\n')[2];
-    final classMethodName =
-        callerFrame.substring(8).substringBeforeLast('(').trim();
-    return '[$classMethodName]';
+    try {
+      final stackTrace = StackTrace.current;
+      final callerFrame = stackTrace.toString().split('\n')[2];
+      final classMethodName =
+          callerFrame.substring(8).substringBeforeLast('(').trim();
+      return '[$classMethodName]';
+    } catch (_) {
+      return '[unknown]';
+    }
   }
 
   @override
