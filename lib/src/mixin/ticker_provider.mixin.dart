dd5422bce8b3cd1d3c05e47910763b54c43e211b
yohom <yohombao@qq.com>
2025-06-28 19:54:13 +0800

    enhance: bloc去除immutable, 到目前为止都没什么用, 徒增警告

--- lib/src/mixin/ticker_provider.mixin.dart
+++ lib/src/mixin/ticker_provider.mixin.dart
@@ -8,7 +8,7 @@ import 'package:flutter/widgets.dart';
 /// 跟[State]提供的[TickerProvider]有点区别, [SingleTickerProviderBLoCMixin]去掉了对
 /// 动画是否在屏幕外的检测, 因为[BuildContext]无法获取
 mixin SingleTickerProviderBLoCMixin on BLoC implements TickerProvider {
-  late final Ticker _ticker;
+  Ticker? _ticker;
 
   @override
   Ticker createTicker(TickerCallback onTick) {
@@ -20,7 +20,7 @@ mixin SingleTickerProviderBLoCMixin on BLoC implements TickerProvider {
 
   @override
   void dispose() {
-    _ticker.dispose();
+    _ticker?.dispose();
     super.dispose();
   }
 }
