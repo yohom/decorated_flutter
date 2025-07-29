77acae92a011bab602ae0d39c881c60faa77b726
yohom <yohombao@qq.com>
2025-07-29 09:13:47 +0800

    enhance: TransparentRoute默认动画时长0

--- lib/src/ui/route/transparent_route.route.dart
+++ lib/src/ui/route/transparent_route.route.dart
@@ -21,7 +21,7 @@ class TransparentRoute extends PageRoute<void> {
   bool get maintainState => true;
 
   @override
-  Duration get transitionDuration => const Duration(milliseconds: 350);
+  Duration get transitionDuration => Duration.zero;
 
   @override
   Widget buildPage(
