3bc738af353f2d0bbddda59c9924d47d4ae42c21
yohom <yohombao@qq.com>
2025-09-25 11:00:47 +0800

    enhance: 删除可以被collection等价替换的扩展

--- lib/src/extension/iterable.x.dart
+++ lib/src/extension/iterable.x.dart
@@ -42,14 +42,6 @@ extension IterableX<T> on Iterable<T> {
     }
   }
 
-  T? firstWhereOrNull(bool Function(T element) test) {
-    try {
-      return firstWhere(test);
-    } catch (_) {
-      return null;
-    }
-  }
-
   T? getOrNull(int? index) {
     if (index == null) return null;
     try {
