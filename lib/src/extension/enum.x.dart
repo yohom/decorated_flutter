fa27bde11b4dfd151688d455482c2eafd960c532
byh <382146139@qq.com>
2026-06-12 09:39:51 +0800

    chore: 枚举类简化

--- lib/src/extension/enum.x.dart
+++ lib/src/extension/enum.x.dart
@@ -1,5 +1,3 @@
-import '../utils/utils.export.dart';
-
 extension EnumX on Enum {
   bool operator >(Enum other) {
     return index > other.index;
@@ -15,38 +13,5 @@ extension EnumX on Enum {
 
   bool operator <=(Enum other) {
     return index <= other.index;
-  }
-}
-
-extension ConstellationX on Constellation {
-  String get name {
-    return const [
-      '摩羯座',
-      '水瓶座',
-      '双鱼座',
-      '白羊座',
-      '金牛座',
-      '双子座',
-      '巨蟹座',
-      '狮子座',
-      '处女座',
-      '天秤座',
-      '天蝎座',
-      '射手座',
-      '未知',
-    ][index];
-  }
-}
-
-extension GenderX on Gender {
-  String get name {
-    switch (this) {
-      case Gender.male:
-        return '男';
-      case Gender.female:
-        return '女';
-      case Gender.unknown:
-        return '保密';
-    }
   }
 }
