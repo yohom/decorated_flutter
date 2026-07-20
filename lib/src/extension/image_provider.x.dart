4c30712a2c7000baec2bbafed36278f75f9ccfa6
byh <382146139@qq.com>
2026-07-20 13:59:58 +0800

    fix: ImageProvider获取imageData扩展优化

--- lib/src/extension/image_provider.x.dart
+++ lib/src/extension/image_provider.x.dart
@@ -1,8 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:decorated_flutter/decorated_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
-extension AssetImageX on ImageProvider {
+extension ImageProviderX on ImageProvider {
   Future<Uint8List> get imageData async {
     final _this = this;
     if (_this is AssetImage) {
@@ -19,6 +20,10 @@ extension AssetImageX on ImageProvider {
           .asUint8List();
     } else if (_this is MemoryImage) {
       return _this.bytes;
+    } else if (_this is FileImage) {
+      return _this.file.readAsBytes();
+    } else if (_this is ImageViewProvider) {
+      return _this.imageData;
     } else {
       throw '暂未支持的Image类型 ${_this.runtimeType}';
     }
